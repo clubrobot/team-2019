1a5efd9826125075d8810caf055eac49b5a409bc
Francois Gauthier-Clerc <francois.gauthier-clerc@insa-rennes.fr>
2018-07-02 20:42:03 +0200

    add some lock on all send methods.

--- raspberrypi/common/serialutils.py
+++ raspberrypi/common/serialutils.py
@@ -2,120 +2,138 @@
 #-*- coding: utf-8 -*-
 
 import struct
+from threading import RLock
+import warnings
 
+class SerialBufferTimeout(UserWarning, TimeoutError): pass
 
 
 class SerialBuffer:
-    def __init__(self,send_method, extern_buffer_size):
+    def __init__(self, send_method, extern_buffer_size):
         self.stack_bytes = bytearray()
         self.rawsend = send_method
         self.buffer_size = extern_buffer_size
         self.bytes_sended = 0
+        self.lock = RLock()
 
-    def send(self,bytes):
+    def send(self, bytes, force=False, timeout=1):
+        if not self.lock.acquire(blocking=not force, timeout=timeout) and force:
+            warnings.warn("Bytes : {} can't be sended, timeout !".format(bytes), SerialBufferTimeout)
+            return 0
         if self.bytes_sended >= self.buffer_size:
             self.stack_bytes += bytes
+            res = 0
         else:
             to_send, to_store = bytearray(bytes)[:min(self.buffer_size-self.bytes_sended,len(bytes))], bytearray(bytes)[min(self.buffer_size-self.bytes_sended,len(bytes)):]
             self.stack_bytes+=to_store
             self.bytes_sended += len(to_send)
-            return self.rawsend(to_send)
+            res = self.rawsend(to_send)
+        self.lock.release()
+        return res
 
-    def direct_send(self,bytes):
-        return self.rawsend(bytes)
+    def direct_send(self, bytes, timeout=1):
+        if not self.lock.acquire(blocking=True, timeout=timeout):
+            warnings.warn("Bytes : {} can't be sended, timeout !".format(bytes), SerialBufferTimeout)
+            raise TimeoutError()
+            return 0
+        res = self.rawsend(bytes)
+        self.lock.release()
 
     def reset(self, *args):
+        self.lock.acquire(blocking=True)
         self.bytes_sended = 0
         bytes_to_send = bytes(self.stack_bytes)
         self.stack_bytes = bytearray()
-        self.send(bytes_to_send)
+        self.send(bytes_to_send, force=True, timeout=-1)
+        self.lock.release()
+
 
 
 class AbstractType:
 
-	def __call__(self, value):
-		return self.to_bytes(value)
+    def __call__(self, value):
+        return self.to_bytes(value)
 
 
 class IntegerType(AbstractType):
 
-	def __init__(self, length, byteorder, signed):
-		self.length    = length
-		self.byteorder = byteorder
-		self.signed    = signed
+    def __init__(self, length, byteorder, signed):
+        self.length    = length
+        self.byteorder = byteorder
+        self.signed    = signed
 
-	def to_bytes(self, integer):
-		return int.to_bytes(integer, length = self.length, byteorder = self.byteorder, signed = self.signed)
+    def to_bytes(self, integer):
+        return int.to_bytes(integer, length = self.length, byteorder = self.byteorder, signed = self.signed)
 
-	def from_bytes(self, rawbytes):
-		return int.from_bytes(rawbytes[:self.length], byteorder = self.byteorder, signed = self.signed)
+    def from_bytes(self, rawbytes):
+        return int.from_bytes(rawbytes[:self.length], byteorder = self.byteorder, signed = self.signed)
 
 
 class FloatType(AbstractType):
 
-	def __init__(self, standard):
-		self.standard = standard # May be 'f' (float) or 'd' (double)
+    def __init__(self, standard):
+        self.standard = standard # May be 'f' (float) or 'd' (double)
 
-	def to_bytes(self, real):
-		return struct.pack(self.standard, real)
-	
-	def from_bytes(self, rawbytes):
-		length = {'f': 4, 'd': 8}[self.standard]
-		return struct.unpack(self.standard, rawbytes[:length])[0]
+    def to_bytes(self, real):
+        return struct.pack(self.standard, real)
+
+    def from_bytes(self, rawbytes):
+        length = {'f': 4, 'd': 8}[self.standard]
+        return struct.unpack(self.standard, rawbytes[:length])[0]
 
 
 class StringType(AbstractType):
 
-	def __init__(self, encoding):
-		self.encoding = encoding
+    def __init__(self, encoding):
+        self.encoding = encoding
 
-	def to_bytes(self, string):
-		return string.encode(self.encoding) + b'\0'
+    def to_bytes(self, string):
+        return string.encode(self.encoding) + b'\0'
 
-	def from_bytes(self, rawbytes):
-		index = rawbytes.index(b'\0')
-		return rawbytes[:index].decode(self.encoding)
+    def from_bytes(self, rawbytes):
+        index = rawbytes.index(b'\0')
+        return rawbytes[:index].decode(self.encoding)
 
 
 class Deserializer:
-	
-	def __init__(self, rawbytes):
-		self.remaining = rawbytes
 
-	def __copy__(self):
-		return Deserializer(self.remaining)
+    def __init__(self, rawbytes):
+        self.remaining = rawbytes
 
-	def read(self, *types):
-		output = list()
+    def __copy__(self):
+        return Deserializer(self.remaining)
 
-		for T in types:
-			data   = T.from_bytes(self.remaining)
-			length = len(T.to_bytes(data))
-			output.append(data)
-			self.remaining = self.remaining[length:]
+    def read(self, *types):
+        output = list()
 
-		return tuple(output) if len(output) > 1 else output[0]
+        for T in types:
+            data   = T.from_bytes(self.remaining)
+            length = len(T.to_bytes(data))
+            output.append(data)
+            self.remaining = self.remaining[length:]
+
+        return tuple(output) if len(output) > 1 else output[0]
 
 
 if __name__ == '__main__':
-	from pprint import pprint
-	
-	char_t   = IntegerType(1, 'little', True)
-	byte_t   = IntegerType(1, 'little', False)
-	int_t    = IntegerType(2, 'little', True)
-	uint_t   = IntegerType(4, 'little', False)
-	string_t = StringType('utf-8')
-	float_t  = FloatType('f')
+    from pprint import pprint
 
-	out = Deserializer(byte_t  (10) +
-	                   char_t  (ord('X')) +
-	                   uint_t  (123456) +
-	                   int_t   (-789) +
-	                   string_t('hello') +
-	                   float_t (987.654))
+    char_t   = IntegerType(1, 'little', True)
+    byte_t   = IntegerType(1, 'little', False)
+    int_t    = IntegerType(2, 'little', True)
+    uint_t   = IntegerType(4, 'little', False)
+    string_t = StringType('utf-8')
+    float_t  = FloatType('f')
 
-	b, c, u, i = out.read(byte_t, char_t, uint_t, int_t)
-	s, f       = out.read(string_t, float_t)
+    out = Deserializer(byte_t  (10) +
+                       char_t  (ord('X')) +
+                       uint_t  (123456) +
+                       int_t   (-789) +
+                       string_t('hello') +
+                       float_t (987.654))
 
-	print(b, c, u, i, s, f, len(out.remaining))
-	print(char_t(82))
+    b, c, u, i = out.read(byte_t, char_t, uint_t, int_t)
+    s, f       = out.read(string_t, float_t)
+
+    print(b, c, u, i, s, f, len(out.remaining))
+    print(char_t(82))
