b4c9edf6f15d5dddf051fb9b9fdd0360e1f5e172
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2018-08-06 09:56:39 +0200

    Add GNU header

--- arduino/common/SerialTalks.cpp
+++ arduino/common/SerialTalks.cpp
@@ -3,6 +3,16 @@
 #include <EEPROM.h>
 
 
+// This library is free software from Club robot Insa Rennes sources; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+
 // Global instance
 
 SerialTalks talks;
