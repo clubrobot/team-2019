106b32784eb41ebe62924e1a8777c08baa2101e9
mathieu.percelay <mathieup@mailoo.org>
2018-10-02 21:09:56 +0200

    Add test script

---
+++ raspberrypi/common/test-roadmap.py
@@ -0,0 +1,14 @@
+from roadmap import RoadMap
+from geogebra import Geogebra
+import time
+
+begin = time.time()
+global count
+count = 0
+
+print("GEOGEBRA")
+geo = Geogebra('../robots/bornibus.ggb')
+print("FIN GEOGEBRA ", time.time() - begin)
+print("ROADMAP ", time.time() - begin)
+rm = RoadMap.load(geo)
+print("Fin Chargement ", time.time() - begin)
