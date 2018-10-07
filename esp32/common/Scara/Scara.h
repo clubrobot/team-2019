abd65ef73fd2b76aacfde00fc4ab4ca38259d35d
Mathis Lécrivain <matlecr35@gmail.com>
2018-10-07 23:17:06 +0200

    [Under Progress] Add Scara arm class
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/common/Scara/Scara.h
+++ esp32/common/Scara/Scara.h
@@ -1,14 +1,40 @@
 #ifndef __SCARA_H
 #define __SCARA_H
+#include "Joint.h"
+#include <Arduino.h>
+#include <tuple>
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+typedef std::tuple<double,double> coords_t;
 
 class Scara
 {
+	private:
 
+		coords_t m_origin;
+		coords_t m_tool;
+		coords_t m_joints;
+
+		double m_l1;
+		double m_l2;
+		double m_lsq;
+
+		Joint Theta1_joint = Joint(-M_PI,M_PI,1,-1,1,-1,1);
+		Joint Theta2_joint = Joint(-M_PI,M_PI,1,-1,1,-1,1);
 
 	public:
-		Scara();
-		~Scara();
-	
+		Scara(double l1, double l2, coords_t joints, coords_t origin);
+		coords_t forward_kinematics(coords_t joints);
+
+		coords_t inverse_kinematics(coords_t tool);
+
+		coords_t get_tool(void);
+
+		coords_t get_joints(void);
+
+
 };
 
 
