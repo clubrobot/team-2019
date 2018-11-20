e4622203d0f0e7317e560636d1a55d8d88db357d
Mathis Lécrivain <matlecr35@gmail.com>
2018-11-20 16:24:59 +0100

    Move func to private
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/common/IK/ArmManager.h
+++ esp32/common/IK/ArmManager.h
@@ -29,14 +29,10 @@ class ArmManager
     public:
 
         void        init(workspace_t ws_front, workspace_t ws_back, double time_resolution);
-        workspace_t workspace_containing_position(coords_t position);
-        bool        workspace_within_constraints(workspace_t workspace);
         workspace_t clip_workspace_to_constraints(workspace_t workspace);
-        bool        position_within_workspace(coords_t position, workspace_t workspace);
         coords_t    workspace_center(workspace_t workspace);
         path_t      go_to(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel);
         path_t      go_home(coords_t start_pos, coords_t start_vel);
-        path_t      goto_workspace(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel, workspace_t new_workspace);
         path_t      goto_position(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel);
         double      estimated_time_of_arrival(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel);
 
@@ -44,7 +40,11 @@ class ArmManager
         
     private:
 
-        path_t merge_trajectories(path_t traj_a, path_t traj_b);
+        path_t      merge_trajectories(path_t traj_a, path_t traj_b);
+        workspace_t workspace_containing_position(coords_t position);
+        bool        workspace_within_constraints(workspace_t workspace);
+        bool        position_within_workspace(coords_t position, workspace_t workspace);
+        path_t      goto_workspace(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel, workspace_t new_workspace);
 
         workspace_t m_ws_front;
         workspace_t m_ws_back;
