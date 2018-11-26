f917ef7ac9e7b12f5db0f980aeaef56405c83f1b
Mathis Lécrivain <matlecr35@gmail.com>
2018-11-26 09:39:16 +0100

    update Trajectory manager test main
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/Test_ArmManager/main.ino
+++ esp32/Test_ArmManager/main.ino
@@ -11,28 +11,19 @@
 
 #define LINK1_LEN 10.0
 #define LINK2_LEN 10.0
-#define LINK3_LEN 5.0
+#define LINK3_LEN 3.8
 
 #define ORIGIN_X 0.0
 #define ORIGIN_Y 0.0
 #define ORIGIN_PHI 0.0
 
 #define ID1 1
-#define ID2 2
-#define ID3 3
-
-#define USE_SHIFTREG 1
-
-#define LATCHPIN	10
-#define CLOCKPIN	12
-#define DATAPIN		5 
+#define ID2 3
+#define ID3 2
 
 /* debug */
 template<typename T> ostream& operator<< (ostream& out, const vector<T>& v);
 
-ShiftRegister shift;
-
-ArmManager arm_manager(0.2);
 TrajectoryManager traj_manager;
 
 void setup()
@@ -43,40 +34,53 @@ void setup()
     talks.bind(MOVE_DIRECTLY_OPCODE, MOVE_DIRECTLY);
     talks.bind(MOVE_PATH_OPCODE, MOVE_PATH);
     talks.bind(IS_ARRIVED_OPCODE, IS_ARRIVED);
+    talks.bind(GO_HOME_OPCODE, GO_HOME);
 
-    pinMode(LATCHPIN, OUTPUT);
-    pinMode(CLOCKPIN, OUTPUT);
-    pinMode(DATAPIN , OUTPUT);
 
-    shift.attach(LATCHPIN,CLOCKPIN,DATAPIN);
 
-    workspace_t ws_side = {-10,
-                        10,
-                        abs(10 - 10),
-                        abs(10 + 10),
-                        1.0};
+    workspace_t ws_side = {-20.5,
+                        20.5,
+                        -20.5,
+                        20.5,
+                        -1.0};
 
-    workspace_t ws_front = {-abs(10 + 10),
-                        -abs(10 - 10),
-                        -10,
-                        10,
-                        1.0};
+    workspace_t ws_front = {-abs(20 + 20),
+                        -abs(20 - 20),
+                        -20,
+                        20,
+                        -1.0};
 
-    /* Configure Arm Manager */
-    arm_manager.init_workspace(ws_side, ws_front);                      /*      init workspaces      */
-    arm_manager.set_origin(ORIGIN_X, ORIGIN_Y, ORIGIN_PHI);             /*      set arm origin       */
-    arm_manager.attach(ID1, ID2, ID3, LINK1_LEN, LINK2_LEN, LINK3_LEN); /*      attach ax12 motors   */
-    arm_manager.init_arm(8,8,0);                                        /*      init arm at pos      */
+    /* init TrajectoryManager */
+    traj_manager.init_workspace(ws_side, ws_front);                      /*      init workspaces      */
+    traj_manager.set_origin(ORIGIN_X, ORIGIN_Y, ORIGIN_PHI);             /*      set arm origin       */
+    traj_manager.attach(ID1, ID2, ID3, LINK1_LEN, LINK2_LEN, LINK3_LEN); /*      attach ax12 motors   */
+    traj_manager.init_arm(5,5,0);                                        /*      init arm at pos      */
+
     
-    /* begin TrajectoryManager */
-    // traj_manager.goto_path(5.0,5.0,0.0);
-
-    // traj_manager.goto_directly(9.0,5.0,0.0);
 
 }
 
 void loop()
 {  
+    traj_manager.goto_directly(20.0, 3.8, M_PI/2);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/3);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/4);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/6);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/0);
+    delay(1000);
+
+    traj_manager.goto_directly(20.0, 3.8, M_PI/6);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/4);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/3);
+    delay(1000);
+    traj_manager.goto_directly(20.0, 3.8, M_PI/2);
+    delay(1000);
     talks.execute();
 }
 
--- raspberrypi/test.py
+++ raspberrypi/test.py
@@ -2,7 +2,7 @@ from common.serialtalks import *
 from math import pi
 import time
 
-s = SerialTalks("/tmp/arduino/esp32")
+s = SerialTalks("/dev/tty.SLAB_USBtoUART")
 s.connect();
 
 MOVE_DIRECTLY_OPCODE    = 0X10
@@ -15,38 +15,13 @@ y	= 10
 phi = 0
 
 
-output = s.execute(MOVE_DIRECTLY_OPCODE,FLOAT(5.0), FLOAT(5.0), FLOAT(0.0))
+output = s.execute(MOVE_DIRECTLY_OPCODE,FLOAT(15.0), FLOAT(10.0), FLOAT(0.0))
 trajectory_time = output.read(FLOAT)
 print(trajectory_time)
 
-# Wait for 5 seconds
+print("wait")
 time.sleep(trajectory_time)
-
-output = s.execute(IS_ARRIVED_OPCODE)
-is_arrived = output.read(INT)
-
-while (is_arrived is not 0X00):
-    output = s.execute(IS_ARRIVED_OPCODE)
-    is_arrived = output.read(INT)
-    time.sleep(0.100)
-
-
-output = s.execute(MOVE_PATH_OPCODE,FLOAT(9.0), FLOAT(5.0), FLOAT(0.0))
-trajectory_time = output.read(FLOAT)
-print(trajectory_time)
-
-# Wait for 5 seconds
-time.sleep(trajectory_time)
-
-output = s.execute(IS_ARRIVED_OPCODE)
-is_arrived = output.read(INT)
-
-while (is_arrived is not 0X00):
-    output = s.execute(IS_ARRIVED_OPCODE)
-    is_arrived = output.read(INT)
-    time.sleep(0.100)
-
-
+print("next pos")
 
 
 
