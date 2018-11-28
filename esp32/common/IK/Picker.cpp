176651e6e19f1d5f7dc0b45fed5154b1f2bf5e7e
Mathis Lécrivain <matlecr35@gmail.com>
2018-11-28 14:32:48 +0100

    add mutex into picker to secure class
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/common/IK/Picker.cpp
+++ esp32/common/IK/Picker.cpp
@@ -14,6 +14,8 @@ namespace IK
 
 void Picker::init(double l1, double l2, double l3, joints_t joints, coords_t origin, int elbow_or)
 {
+    m_mutex.acquire();
+
     m_joints 	= joints;
 	m_origin	= origin;
 	m_tool	 	= get_tool();
@@ -28,40 +30,64 @@ void Picker::init(double l1, double l2, double l3, joints_t joints, coords_t ori
 	phi_axis = {-(5*M_PI)/6, (5*M_PI)/6 , -1, 1, -1, 1};
 
     m_flip_elbow = elbow_or;
+
+    m_mutex.release();
 }
 
 void Picker::flip_elbow(int elbow)
 {
+    m_mutex.acquire();
+
     m_flip_elbow = elbow;
+
+    m_mutex.release();
 }
 
 ik_error_t Picker::get_error()
 {
-    m_error.th1_error = Theta1_joint.get_error();
-    m_error.th2_error = Theta2_joint.get_error();
-    m_error.th3_error = Theta3_joint.get_error();
-    return m_error;
+    m_mutex.acquire();
+    ik_error_t ret;
+
+    ret.th1_error = Theta1_joint.get_error();
+    ret.th2_error = Theta2_joint.get_error();
+    ret.th3_error = Theta3_joint.get_error();
+
+    m_error = ret; 
+
+    m_mutex.release();
+    return ret;
 }
 
 void Picker::reset_error()
 {
+    m_mutex.acquire();
+
     Theta1_joint.reset_error();
     Theta2_joint.reset_error();
     Theta3_joint.reset_error();
+
+    m_mutex.release();
 }
-        
-Picker::~Picker()
-{
-    
-}
+
 coords_t Picker::forward_kinematics(joints_t joints)
 {
+    m_mutex.acquire();
+
+    coords_t ret;
 	m_joints = joints;
-	return get_tool();
+
+    m_mutex.release();
+
+	ret = get_tool();
+
+    return ret;
 }
 
 joints_t Picker::inverse_kinematics(coords_t tool)
 {
+    m_mutex.acquire();
+    joints_t ret;
+
     double dotx = (m_tool.x - m_origin.x) - (m_l3 * cos(m_tool.phi));
     double doty = (m_tool.y - m_origin.y) - (m_l3 * sin(m_tool.phi));
 
@@ -69,17 +95,30 @@ joints_t Picker::inverse_kinematics(coords_t tool)
 
     if( norm > pow((m_l1 + m_l2 + m_l3),2) || norm < pow((m_l1 - m_l2 - m_l3),2))
     {
+        m_mutex.release();
         m_tool = get_tool();
+        m_joints = get_joints();
+
+        m_mutex.acquire();
+        ret = m_joints;
+        m_mutex.release();
+    }
+    else
+    {
+        m_tool = tool;
+        m_mutex.release();
+        m_joints = get_joints();
+
+        m_mutex.acquire();
+        ret = m_joints;
+        m_mutex.release();
     }      
-
-    m_tool = tool;
-    m_joints = get_joints();
-
-    return m_joints;
+    return ret;
 }
 
 coords_t Picker::get_tool(void)
 {
+    m_mutex.acquire();
     coords_t new_cords;
 
     new_cords.x     = m_l1 * cos(m_joints.th1) + m_l2 * cos(m_joints.th1 + m_joints.th2) + m_l3 * cos(m_joints.th1 + m_joints.th2 + m_joints.th3);
@@ -89,12 +128,13 @@ coords_t Picker::get_tool(void)
     new_cords.x     += m_origin.x;
     new_cords.y     += m_origin.y;
     new_cords.phi   += m_origin.phi;
-
+    m_mutex.release();
     return new_cords;
 }
 
 joints_t Picker::get_joints(void)
 {
+    m_mutex.acquire();
     joints_t new_joints;
 
     double dotx,doty,costh,sinth,k1,k2;
@@ -116,6 +156,7 @@ joints_t Picker::get_joints(void)
     new_joints.th2 = std::fmod((new_joints.th2 + (M_PI)) , 2*M_PI) - M_PI; // Stay between -pi and pi
     new_joints.th3 = std::fmod((new_joints.th3 + (M_PI)) , 2*M_PI) - M_PI; // Stay between -pi and pi
 
+    m_mutex.release();
     return new_joints;
 }
 
@@ -124,6 +165,7 @@ detailed_pos_t Picker::get_detailed_pos(void)
     /*
         Returns origin, position of end of link 1, position of end of link 2
     */
+    m_mutex.acquire();
     detailed_pos_t new_pos;
 
     new_pos.link1.x = m_l1 * cos(m_joints.th1) + m_origin.x;
@@ -134,7 +176,7 @@ detailed_pos_t Picker::get_detailed_pos(void)
 
     new_pos.origin  = m_origin;
     new_pos.tool    = m_tool;
-
+    m_mutex.release();
     return new_pos;
 }
 
@@ -143,6 +185,8 @@ matrix_t Picker::compute_jacobian(void)
     /*
         Returns jacobian matrix at current state
     */
+    m_mutex.acquire();
+    matrix_t ret;
 
     double dx_dth1 = - m_l1 * sin(m_joints.th1) - m_l2 * sin(m_joints.th1 + m_joints.th2) - m_l3 * sin(m_joints.th1 + m_joints.th2 + m_joints.th3);
 
@@ -156,7 +200,10 @@ matrix_t Picker::compute_jacobian(void)
 
     double dy_dth3 = m_l3 * cos(m_joints.th1 + m_joints.th2 + m_joints.th3);
 
-    return m_matrix.createMatrix33(dx_dth1, dx_dth2, dx_dth3, dy_dth1, dy_dth2, dy_dth3, 1.0, 1.0, 1.0);
+    ret = m_matrix.createMatrix33(dx_dth1, dx_dth2, dx_dth3, dy_dth1, dy_dth2, dy_dth3, 1.0, 1.0, 1.0);
+    m_mutex.release();
+    return ret;
+
 }
 
 coords_t Picker::get_tool_vel(joints_t joints_vel)
@@ -164,12 +211,14 @@ coords_t Picker::get_tool_vel(joints_t joints_vel)
     /*
         Computes current tool velocity using jacobian
     */
+    m_mutex.acquire();
     coords_t new_vel;
 
     matrix_t jt_vel = m_matrix.createMatrix31(joints_vel.th1, joints_vel.th2, joints_vel.th3);
 
+    m_mutex.release();
     matrix_t jacobian = compute_jacobian();
-
+    m_mutex.acquire();
     matrix_t tl_vel = m_matrix.multMatrix33x13(jacobian , jt_vel);
 
     new_vel.x   = tl_vel[0][0];
@@ -179,7 +228,7 @@ coords_t Picker::get_tool_vel(joints_t joints_vel)
     m_matrix.free(jt_vel);
     m_matrix.free(jacobian);
     m_matrix.free(tl_vel);
-
+    m_mutex.release();
     return new_vel;
 }
 
@@ -188,18 +237,21 @@ joints_t Picker::get_joints_vel(coords_t tool_vel)
     /*
         Computes current tool velocity using jacobian
     */
+    m_mutex.acquire();
     joints_t vel;
     vel.th1 = 0;
     vel.th2 = 0;
     vel.th3 = 0;
 
     matrix_t tool_v = m_matrix.createMatrix31(tool_vel.x, tool_vel.y, tool_vel.phi);
-
+    m_mutex.release();
     matrix_t jacobian = compute_jacobian();
-
+    m_mutex.acquire();
     if (m_matrix.norm(tool_v) < EPSILON)
+    {
+        m_mutex.release();
         return vel;
-
+    }
     if (abs(m_matrix.det(jacobian)) < EPSILON)
     {
         //singularity
@@ -215,11 +267,13 @@ joints_t Picker::get_joints_vel(coords_t tool_vel)
     m_matrix.free(jacobian);
     m_matrix.free(joints_vel);
 
+    m_mutex.release();
     return vel;
 }
 
 path_t Picker::get_path(coords_t start_pos, coords_t start_vel, coords_t target_pos, coords_t target_vel, double delta_t)
 {
+
     joints_t start_joints_pos = inverse_kinematics(start_pos);
         
     joints_t start_joints_vel = get_joints_vel(start_vel);
@@ -227,12 +281,11 @@ path_t Picker::get_path(coords_t start_pos, coords_t start_vel, coords_t target_
     joints_t target_joints_pos = inverse_kinematics(target_pos);
     joints_t target_joints_vel = get_joints_vel(target_vel);
 
-        
     double tf_sync = synchronisation_time(start_joints_pos,
                                             start_joints_vel,
                                             target_joints_pos,
                                             target_joints_vel);
-
+    m_mutex.acquire();
     //get_path
     path_t new_path;
 
@@ -256,7 +309,7 @@ path_t Picker::get_path(coords_t start_pos, coords_t start_vel, coords_t target_
                                                 target_joints_vel.th3,
                                                 tf_sync,
                                                 delta_t);
-
+    m_mutex.release();
     return new_path;
 }
 
@@ -266,7 +319,7 @@ double Picker::synchronisation_time(joints_t start_pos, joints_t start_vel, join
     Return largest time to destination to use slowest joint as synchronisation
         reference
     */
-
+    m_mutex.acquire();
     // Compute time to destination for all joints
     trajectory_time_t t_theta1 = Theta1_joint.time_to_destination(start_pos.th1, start_vel.th1, target_pos.th1, target_vel.th1);
 
@@ -275,7 +328,7 @@ double Picker::synchronisation_time(joints_t start_pos, joints_t start_vel, join
     trajectory_time_t t_theta3 = Theta3_joint.time_to_destination(start_pos.th3, start_vel.th3, target_pos.th3, target_vel.th3);
 
     double maxi = max(t_theta1.tf, t_theta2.tf);
-    
+    m_mutex.release();
     return max(maxi, t_theta3.tf);
 }
 
