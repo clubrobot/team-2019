3381d4f83f74971eb81d181f2f6d1ab9e30a468e
Mathis Lécrivain <matlecr35@gmail.com>
2018-12-07 19:36:48 +0100

    correct error on Picker class
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/common/IK/Picker.h
+++ esp32/common/IK/Picker.h
@@ -54,9 +54,9 @@ class Picker
 		double m_l3;
 		double m_lsq;
 
-		Joint Theta1_joint = Joint(0, 		0	 ,  M_PI	 , -2, 2, -2, 2);
-		Joint Theta2_joint = Joint(1, -(5*M_PI)/6, (5*M_PI)/6, -2, 2, -2, 2);
-		Joint Theta3_joint = Joint(2, -(5*M_PI)/6, (5*M_PI)/6, -2, 2, -2, 2);
+		Joint Theta1_joint = Joint(0, 		0	 ,  M_PI	 , -5, 5, -25, 25);
+		Joint Theta2_joint = Joint(1, -(5*M_PI)/6, (5*M_PI)/6, -5, 5, -25, 25);
+		Joint Theta3_joint = Joint(2, -(5*M_PI)/6, (5*M_PI)/6, -5, 5, -25, 25);
 
 		Matrix3 m_matrix;
 
