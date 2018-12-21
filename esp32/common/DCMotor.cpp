3c8b621275d1a50f979819d3d198851de913adbc
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2018-12-21 16:02:09 +0100

    Add mutex on DCMotor class.

--- esp32/common/DCMotor.cpp
+++ esp32/common/DCMotor.cpp
@@ -23,6 +23,7 @@ void DCMotor::attach(int EN, int PWM, int PWMChanel,int freq, int DIR)
 
 void DCMotor::update()
 {
+	m_mutex.acquire();
 	if (m_velocity != 0)
 	{
 		int PWM = m_velocity / (2 * M_PI * m_wheelRadius) * m_constant * 255;
@@ -36,41 +37,52 @@ void DCMotor::update()
 	{
 		digitalWrite(m_EN, LOW);
 	}
+	m_mutex.release();
 }
 
 float DCMotor::getMaxVelocity() const
 {
+	m_mutex.acquire();
 	return abs((2 * M_PI * m_wheelRadius) / m_constant) * m_maxPWM;
+	m_mutex.release();
 }
 
 void DCMotor::load(int address)
 {
+	m_mutex.acquire();
 	EEPROM.get(address, m_wheelRadius); address += sizeof(m_wheelRadius);
 	EEPROM.get(address, m_constant);    address += sizeof(m_constant);
 	EEPROM.get(address, m_maxPWM);      address += sizeof(m_maxPWM);
+	m_mutex.release();
 }
 
 void DCMotor::save(int address) const
 {
+	m_mutex.acquire();
 	EEPROM.put(address, m_wheelRadius); address += sizeof(m_wheelRadius);
 	EEPROM.put(address, m_constant);    address += sizeof(m_constant);
 	EEPROM.put(address, m_maxPWM);      address += sizeof(m_maxPWM);
 	EEPROM.commit();
+	m_mutex.release();
 }
 
 void DCMotorsDriver::attach(int RESET, int FAULT)
 {
+	m_mutex.acquire();
 	m_RESET = RESET;
 	m_FAULT = FAULT;
 	pinMode(m_RESET, OUTPUT);
 	pinMode(m_FAULT, INPUT);
+	m_mutex.release();
 }
 
 void DCMotorsDriver::reset()
 {
+	m_mutex.acquire();
 	digitalWrite(m_RESET, LOW);
 	delayMicroseconds(10); // One may adjust this value.
 	digitalWrite(m_RESET, HIGH);
+	m_mutex.release();
 }
 
 bool DCMotorsDriver::isFaulty()
