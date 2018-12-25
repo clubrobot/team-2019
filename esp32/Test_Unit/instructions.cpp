0672b01cc1e6cd7886eca149077313d965a022ec
Mathis Lécrivain <matlecr35@gmail.com>
2018-12-25 19:54:34 +0100

    add instructions and arm position
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/Test_Unit/instructions.cpp
+++ esp32/Test_Unit/instructions.cpp
@@ -7,23 +7,23 @@
 using namespace IK;
 
 extern TrajectoryManager traj_manager;
+extern coords_t arm_positions[];
 
 void MOVE_DIRECTLY(SerialTalks& talks, Deserializer& input, Serializer& output)
 {
-}
-
-
-void MOVE_PATH(SerialTalks& talks, Deserializer& input, Serializer& output)
-{
-    
+    position_t pos_id = (position_t)input.read<int>();
+    try
+    {
+        traj_manager.goto_directly(arm_positions[pos_id]);
+    }
+    catch(const string& err)
+    {
+        cout << err << endl;
+    }
 }
 
 void IS_ARRIVED(SerialTalks& talks, Deserializer& input, Serializer& output)
 {
     int ret = (int)traj_manager.get_status();
     output.write<int>(ret);
-}
-
-void GO_HOME(SerialTalks& talks, Deserializer& input, Serializer& output)
-{
 }
--- esp32/Test_Unit/instructions.h
+++ esp32/Test_Unit/instructions.h
@@ -5,17 +5,39 @@
 #include "../common/SerialTalks.h"
 
 #define MOVE_DIRECTLY_OPCODE      0x15
-#define MOVE_PATH_OPCODE          0x16
-#define IS_ARRIVED_OPCODE         0x17
-#define GO_HOME_OPCODE            0x18
+#define IS_ARRIVED_OPCODE         0x16
+
+typedef enum
+{
+    HOME                = 0,
+    PUCK_POS            = 1,
+    PUCK_POS_INTER      = 2,
+    TANK_POS_INTER      = 3,
+    MID_POS             = 4,
+    TANK_POS_0          = 5,
+    TANK_POS_1          = 6,
+    TANK_POS_2          = 7,
+    BALANCE_POS_INTER   = 8,
+    BALANCE_POS         = 9,
+}position_t;
+
+static coords_t arm_positions[] = 
+{
+    [HOME]              = {10.0, 15.0, 0},
+    [PUCK_POS]          = {-12.0, 15.0, M_PI},
+    [PUCK_POS_INTER]    = {-8.0, 15.0, M_PI},
+    [TANK_POS_INTER]    = {10.0, 10.0, 0},
+    [MID_POS]           = {0, 23.8, M_PI/2},
+    [TANK_POS_0]        = {10.0, 0.0, -M_PI/2},
+    [TANK_POS_1]        = {10.0, 2.0, -M_PI/2},
+    [TANK_POS_2]        = {10.0, 4.0, -M_PI/2},
+    [BALANCE_POS_INTER] = {10.0, 5.0, 0},
+    [BALANCE_POS]       = {10.0, 5.0, 0},
+};
 
 void MOVE_DIRECTLY(SerialTalks& talks, Deserializer& input, Serializer& output);
 
-void MOVE_PATH(SerialTalks& talks, Deserializer& input, Serializer& output);
-
 void IS_ARRIVED(SerialTalks& talks, Deserializer& input, Serializer& output);
-
-void GO_HOME(SerialTalks& talks, Deserializer& input, Serializer& output);
 
 #endif //__INSTRUCTIONS_H__
 
