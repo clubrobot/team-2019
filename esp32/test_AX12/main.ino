3f31265eca415637e95698b2753cb020ae052c62
Mathis Lécrivain <matlecr35@gmail.com>
2018-12-29 12:38:38 +0100

    add test main
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/test_AX12/main.ino
+++ esp32/test_AX12/main.ino
@@ -30,20 +30,46 @@ void setup()
 
     AX12::SerialBegin(1000000, 5);
 
-    servoax.attach(254);
+    servoax.attach(1);
 
     //servoax.setID(1);
-    //servoax.setSRL(1); // Respond only to READ_DATA instructions
+    servoax.setSRL(2); // Respond only to READ_DATA instructions
     // servoax.setLEDAlarm(32); // max torque only
     // servoax.setShutdownAlarm(32); // max torque only
     // servoax.setMaxTorque(1023);
     // servoax.setEndlessMode(OFF);
     // servoax.hold(OFF);
 
-    servoax.move(150.0);
+    
+    try
+    {
+        servoax.move(150.0);
+    }
+    catch(const AX12error& e)
+    {
+        cout << "id : " <<e.get_id()<< endl;
+    }
+    catch(const AX12Timeout& e)
+    {
+        cout << "id : " <<e.get_id()<< endl;
+    }
+    
+    
 }
 void loop()
 {
-
+    try
+    {
+        cout << servoax.readPosition() << endl;
+    }
+    catch(const AX12error& e)
+    {
+        cout << "id : " <<e.get_id()<< endl;
+    }
+    catch(const AX12Timeout& e)
+    {
+        cout << "id : " <<e.get_id()<< endl;
+    }
+    delay(1000);
 	talks.execute();
 }
