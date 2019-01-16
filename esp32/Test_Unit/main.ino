e648ac9ce4e16fd303f175a091a2c1139460bfc8
Mathis Lécrivain <matlecr35@gmail.com>
2019-01-16 19:26:19 +0100

    change test main
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/Test_Unit/main.ino
+++ esp32/Test_Unit/main.ino
@@ -45,18 +45,27 @@ void setup()
     Serial.begin(SERIALTALKS_BAUDRATE);
     talks.begin(Serial);
 
-    /* init Arm Manager */
-    arm_manager.init_workspace(WS_FRONT, WS_BACK);                              /*      init workspaces      */
-    arm_manager.set_origin(ORIGIN);                                             /*      set arm origin       */
-    arm_manager.init_arm(LINK1_LEN, LINK2_LEN, LINK3_LEN, FLIP_ELBOW_BACK);     /*      init arm at pos      */
+    joints_t joint;
 
     /* init Motors */
     AX12::SerialBegin(1000000, 5);
 
-    servoax1.attach(ID1);
-    servoax2.attach(ID2);
-    servoax3.attach(ID3);
+    try
+    {
+        servoax1.attach(ID1);
+        servoax2.attach(ID2);
+        servoax3.attach(ID3);
+    }
+    catch(...)
+    {
+        cout << "err" <<endl;
+    }
 
+    joint.th1 = (servoax1.readPosition() - LINK1_OFFSET)*M_PI / 180;
+    joint.th2 = (servoax2.readPosition() - LINK2_OFFSET)*M_PI / 180;
+    joint.th3 = (servoax3.readPosition() - LINK3_OFFSET)*M_PI / 180;
+
+    cout << joint << endl;
     AX1_PID.setOutputLimits(1, 532);
     AX1_PID.setTunings(1, 0.01 , 0);
 
@@ -78,6 +87,12 @@ void setup()
     AX3.setMOTOR(servoax3);
     AX3.setOFFSET(LINK3_OFFSET);
 
+    /* init Arm Manager */
+    arm_manager.init_workspace(WS_FRONT, WS_BACK);                              /*      init workspaces      */
+    arm_manager.set_origin(ORIGIN);                                             /*      set arm origin       */
+    arm_manager.set_initial_joint_pos(joint);                                   
+    arm_manager.init_arm(LINK1_LEN, LINK2_LEN, LINK3_LEN, FLIP_ELBOW_BACK);     /*      init arm at pos      */
+
     /* init traj Manager */
     traj_manager.set_armManager(arm_manager);
     traj_manager.set_Motors(AX1, AX2, AX3);
@@ -87,9 +102,24 @@ void setup()
     traj_manager.move_directly(arm_positions[HOME]);
     traj_manager.move_directly(arm_positions[PUCK_POS_INTER]);
     traj_manager.move_directly(arm_positions[PUCK_POS]);
-    // traj_manager.move_directly(arm_positions[TANK_POS_INTER]);
-    // traj_manager.move_directly(arm_positions[TANK_POS_0]);
-    // traj_manager.move_directly(arm_positions[HOME]);
+    traj_manager.move_directly(arm_positions[PUCK_POS_INTER_1]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER_2]);
+    traj_manager.move_directly(arm_positions[TANK_POS_0]);
+
+    traj_manager.move_directly(arm_positions[PUCK_POS_INTER]);
+    traj_manager.move_directly(arm_positions[PUCK_POS]);
+    traj_manager.move_directly(arm_positions[PUCK_POS_INTER_1]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER_2]);
+    traj_manager.move_directly(arm_positions[TANK_POS_1]);
+
+    traj_manager.move_directly(arm_positions[PUCK_POS_INTER]);
+    traj_manager.move_directly(arm_positions[PUCK_POS]);
+    traj_manager.move_directly(arm_positions[PUCK_POS_INTER_1]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER]);
+    traj_manager.move_directly(arm_positions[TANK_POS_INTER_2]);
+    traj_manager.move_directly(arm_positions[TANK_POS_2]);
     
     
     traj_manager.enable();
--- esp32/test_AX12/main.ino
+++ esp32/test_AX12/main.ino
@@ -9,10 +9,9 @@
 using namespace std;
 using namespace IK;
 
-AX12 servoax;
-MotorWrapper AX1;
-PID AX1_PID;
-
+AX12 servoax1;
+AX12 servoax2;
+AX12 servoax3;
 void setup()
 {
 
@@ -21,29 +20,41 @@ void setup()
     talks.begin(Serial);
 
     AX12::SerialBegin(1000000, 5);
-    servoax.attach(2);
+    servoax1.attach(254);
+    try
+    {
+        servoax1.move(150);
+    }
+    catch(const AX12error &err)
+    {
+        cout<<"error"<<endl;
+    }
+    catch(const AX12Timeout &err)
+    {
+        cout<<"Timeout"<<endl;
+    }
+    
+    
+    try
+    {
+        cout << servoax1.readPosition() << endl;
+    }
+    catch(const AX12error &err)
+    {
+        cout<<"error"<<endl;
+    }
+    catch(const AX12Timeout &err)
+    {
+        cout<<"Timeout"<<endl;
+    }
+   
 
-    AX1_PID.setOutputLimits(1, 1024);
-    AX1_PID.setTunings(3, 0.01 , 0);
+    
 
-    AX1.setPID(AX1_PID);
-    AX1.setMOTOR(servoax);
-    AX1.setOFFSET(60);
-
-    AX1.setMaxAcc(4000);
-    AX1.setMaxDec(4000);
-    AX1.setMaxSpeed(600);
-    AX1.setTimestep(0.010);
-
-    AX1.getRealVel();
-    AX1.setGoalPos(0.0, 500);
-
-    AX1.enable();
+    
     
 }
 void loop()
 {  
-    AX1.update();
-	talks.execute();
 
 }
