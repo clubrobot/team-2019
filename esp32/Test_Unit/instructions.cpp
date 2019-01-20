ca2cbbb5e22cc214b42c5bc9374f65053485ee89
Mathis Lécrivain <matlecr35@gmail.com>
2019-01-20 20:11:42 +0100

    add instructions
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/Test_Unit/instructions.cpp
+++ esp32/Test_Unit/instructions.cpp
@@ -7,13 +7,20 @@
 using namespace IK;
 
 extern TrajectoryManager traj_manager;
-extern coords_t arm_positions[];
 
-void MOVE_DIRECTLY(SerialTalks& talks, Deserializer& input, Serializer& output)
+void ADD_MOVE_POSITION(SerialTalks& talks, Deserializer& input, Serializer& output)
 {
-    
+    int pos_index = input.read<int>();
+
+    traj_manager.move_directly(arm_positions[pos_index]);
+}
+
+void RUN_BATCH(SerialTalks& talks, Deserializer& input, Serializer& output)
+{
+    traj_manager.enable();
 }
 
 void IS_ARRIVED(SerialTalks& talks, Deserializer& input, Serializer& output)
 {
+    traj_manager.is_arrived();
 }
--- esp32/Test_Unit/instructions.h
+++ esp32/Test_Unit/instructions.h
@@ -3,43 +3,15 @@
 
 #include "../common/IK/Picker.h"
 #include "../common/SerialTalks.h"
+#include "arm_position.h"
 
-#define MOVE_DIRECTLY_OPCODE      0x15
-#define IS_ARRIVED_OPCODE         0x16
+#define ADD_MOVE_POSITION_OPCODE    0x15
+#define RUN_BATCH_OPCODE            0X16  
+#define IS_ARRIVED_OPCODE           0x17
 
-typedef enum
-{
-    HOME                = 0,
-    PUCK_POS            = 1,
-    PUCK_POS_INTER      = 2,
-    TANK_POS_INTER      = 3,
-    MID_POS             = 4,
-    TANK_POS_0          = 5,
-    TANK_POS_1          = 6,
-    TANK_POS_2          = 7,
-    BALANCE_POS_INTER   = 8,
-    BALANCE_POS         = 9,
-    TANK_POS_INTER_2    = 10,
-    PUCK_POS_INTER_1    = 11,
-}position_t;
+void ADD_MOVE_POSITION_DIRECTLY(SerialTalks& talks, Deserializer& input, Serializer& output);
 
-static coords_t arm_positions[] = 
-{
-    [HOME]              = {10.0, 10.0, 0},
-    [PUCK_POS]          = {-14.0, 8.2, M_PI},
-    [PUCK_POS_INTER]    = {-10.0, 8.2, M_PI},
-    [TANK_POS_INTER]    = {10.0, 10.0, 0},
-    [MID_POS]           = {0, 23.8, M_PI/2},
-    [TANK_POS_0]        = {7.0, 0.0, -M_PI/2},
-    [TANK_POS_1]        = {7.0, 2.0, -M_PI/2},
-    [TANK_POS_2]        = {7.0, 4.0, -M_PI/2},
-    [BALANCE_POS_INTER] = {7.0, 5.0, 0},
-    [BALANCE_POS]       = {7.0, 5.0, 0},
-    [TANK_POS_INTER_2]  = {7.0, 6.0, -M_PI/2},
-    [PUCK_POS_INTER_1]  = {-13.0, 8.2, M_PI},
-};
-
-void MOVE_DIRECTLY(SerialTalks& talks, Deserializer& input, Serializer& output);
+void RUN_BATCH(SerialTalks& talks, Deserializer& input, Serializer& output);
 
 void IS_ARRIVED(SerialTalks& talks, Deserializer& input, Serializer& output);
 
