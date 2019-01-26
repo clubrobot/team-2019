66e2d8d781453492d22e089a42c8c28fe2872e36
mathieu.percelay <mathieup@mailoo.org>
2019-01-26 15:15:22 +0100

    Correct initialization of Bornibus

--- raspberrypi/robots/setup_bornibus.py
+++ raspberrypi/robots/setup_bornibus.py
@@ -5,5 +5,8 @@ from robots.setup_serialtalks import *
 from daughter_cards.bornibus_actuators import *
 
 gripper = Gripper(manager)
+gripper.open()
 pushers = Pushers(manager)
+pushers.up()
 arm =     Arm(manager)
+arm.up()
