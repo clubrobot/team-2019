5eddda5b9e364bef79c0ef93ec496e48fe57f495
Mathis Lécrivain <matlecr35@gmail.com>
2019-01-29 22:16:20 +0100

    add Test_arm script
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/Test_arm.py
+++ raspberrypi/Test_arm.py
@@ -13,21 +13,42 @@ s = SerialTalks("/dev/tty.SLAB_USBtoUART")
 r = RobotArm(s)
 r.connect()
 
+r.start_pump()
 r.move(data["TAKE_PUCK_INTER"])
 r.move(data["TAKE_PUCK"])
+r.run_batch()
+time.sleep(1)
+
 r.move(data["TANK_POS_INTER"])
 r.move(data["TANK_POS_1"])
+r.run_batch()
+time.sleep(2)
+r.stop_pump()
+time.sleep(2)
 
+r.start_pump()
 r.move(data["TAKE_PUCK_INTER"])
 r.move(data["TAKE_PUCK"])
+r.run_batch()
+time.sleep(1)
+
 r.move(data["TANK_POS_INTER"])
 r.move(data["TANK_POS_2"])
-
-r.move(data["TAKE_PUCK_INTER"])
-r.move(data["TAKE_PUCK"])
-r.move(data["TANK_POS_INTER"])
-r.move(data["TANK_POS_3"])
-
 r.run_batch()
 
-time.sleep(5)
+time.sleep(2)
+
+r.stop_pump()
+time.sleep(2)
+
+r.start_pump()
+r.move(data["TAKE_PUCK_INTER"])
+r.move(data["TAKE_PUCK"])
+r.run_batch()
+time.sleep(1)
+
+r.move(data["TANK_POS_INTER"])
+r.move(data["TANK_POS_3"])
+r.run_batch()
+time.sleep(2)
+r.stop_pump()
