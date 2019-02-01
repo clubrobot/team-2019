a7d45be30cfde04cc66081b9c9cfa7e19ee61213
mathieu.percelay <mathieup@mailoo.org>
2019-02-01 20:04:28 +0100

    Add display management for Bornibus

--- raspberrypi/robots/setup_bornibus.py
+++ raspberrypi/robots/setup_bornibus.py
@@ -2,9 +2,16 @@
 #-*- coding: utf-8 -*-
 
 from robots.setup_serialtalks import *
-from daughter_cards.bornibus_actuators import *
 from robots.setup_wheeledbase import *
+from daughter_cards.bornibus_actuators import *
+from daughter_cards.display import *
+from robots.display_manager import *
 
 gripper = Gripper(manager)
 pushers = Pushers(manager)
 arm =     Arm(manager)
+
+ss = SevenSegments(manager)
+right_eye = LEDMatrix(manager, 1)
+left_eye = LEDMatrix(manager, 2)
+disp_man = DisplayPoints(ss, right_eye, left_eye)
