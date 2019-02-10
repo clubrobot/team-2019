b16123c69a02cb88b402b38bcac97e413c56732d
Mathis Lécrivain <matlecr35@gmail.com>
2019-02-10 18:20:51 +0100

    add First Taking and moving script
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ raspberrypi/robots/TakePuck.py
@@ -0,0 +1,91 @@
+from math import pi
+import time
+import json
+
+from common.geogebra import Geogebra
+from setup_128 import *
+
+
+test_map = Geogebra("TakePuck.ggb")
+print("Map chargée")
+
+print("load points :")
+puck1   = test_map.get("Puck1")
+puck2   = test_map.get("Puck2")
+puck3   = test_map.get("Puck3")
+balance = test_map.get("Balance")
+
+print(puck1)
+print(puck2)
+print(puck3)
+print(balance)
+
+wheeledbase.set_position(755, 2681, 0)
+print("robot placé : ", wheeledbase.get_position())
+
+input()
+
+wheeledbase.goto(*puck1, theta=-pi/2)
+while not wheeledbase.isarrived():
+    print(wheeledbase.get_position())
+    time.sleep(0.1)
+
+print("robot arrivé sur puck1")
+
+input()
+
+arm.take_puck_in_distributor()
+arm.put_in_tank()
+arm.go_home()
+input()
+
+wheeledbase.goto(*puck2, theta=-pi/2)
+while not wheeledbase.isarrived():
+    print(wheeledbase.get_position())
+    time.sleep(0.1)
+
+print("robot arrivé sur puck2")
+
+input()
+
+arm.take_puck_in_distributor()
+arm.put_in_tank()
+arm.go_home()
+input()
+
+wheeledbase.goto(*puck3, theta=-pi/2)
+while not wheeledbase.isarrived():
+    print(wheeledbase.get_position())
+    time.sleep(0.1)
+
+print("robot arrivé sur puck3")
+
+input()
+
+arm.take_puck_in_distributor()
+arm.put_in_tank()
+arm.go_home()
+input()
+
+wheeledbase.goto(*balance, theta=-pi/2)
+while not wheeledbase.isarrived():
+    print(wheeledbase.get_position())
+    time.sleep(0.1)
+
+print("robot arrivé sur Balance")
+
+input()
+
+arm.take_in_tank()
+arm.put_in_balance()
+
+input()
+arm.take_in_tank()
+arm.put_in_balance()
+
+input()
+arm.take_in_tank()
+arm.put_in_balance()
+
+input()
+print("end")
--- raspberrypi/robots/setup_128.py
+++ raspberrypi/robots/setup_128.py
@@ -2,6 +2,8 @@ from robots.setup_wheeledbase import *
 from daughter_cards.RobotArm import *
 import json
 
+wheeledbase.set_velocities(0, 0)
+
 arm = RobotArm(manager)
 arm.setup_tank_size(3)
 arm.go_home()
