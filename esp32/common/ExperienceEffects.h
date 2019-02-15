793ea009e6bf43deede296f2fc8088360c85a303
Antoine Pégné <hello@antoinepegne.fr>
2019-02-15 09:15:02 +0100

    Added pins in Experience board API

--- esp32/common/ExperienceEffects.h
+++ esp32/common/ExperienceEffects.h
@@ -3,6 +3,13 @@
 
 #define NUMPIXEL 60
 #define PINPIXEL 34
+#define PWM_MOTOR 21
+#define EN_MOTOR 27
+#define SELECT_MOTOR 12
+#define SERVO1 32
+#define SERVO2 25
+#define ROTARY 4
+#define RST_DC_DRV 23
 
 class ExperienceEffects
 {
