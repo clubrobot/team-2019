96a89424f6176a8791c4c04c8b9232b2784334d3
thomasmevel <thomas.mevel2@gmail.com>
2019-03-05 20:41:30 +0100

    bornibus team selection

--- raspberrypi/robots/setup_bornibus.py
+++ raspberrypi/robots/setup_bornibus.py
@@ -5,7 +5,14 @@ from robots.setup_serialtalks import *
 from robots.setup_wheeledbase import *
 from daughter_cards.bornibus_actuators import *
 from daughter_cards.display import *
+#from daughter_cards.buttons_card import *
 from robots.display_manager import *
+from robots.buttons_manager import *
+from math import pi
+from common.geogebra import Geogebra
+
+bmap = Geogebra("roadmap.ggb")
+points = dict()
 
 gripper = Gripper(manager)
 gripper.open()
@@ -14,6 +21,20 @@ pushers.up()
 arm =     Arm(manager)
 arm.up()
 
+button_manager = BornibusButtonManager(ButtonCard(manager))
+while buttons_manager.team is none:
+if button_manager.team==1:
+    points[Ini]=bmap.get("IniM")
+    points[Gold1]=bmap.get("Gold1M")
+    points[Gold2]=bmap.get("Gold2M")
+    points[Gold3]=bmap.get("Gold3M")
+    points[Gold4]=bmap.get("Gold4M")
+else if button_manager.team==2:
+    points[Ini]=bmap.get("IniO")
+    points[Gold1]=bmap.get("Gold1O")
+    points[Gold2]=bmap.get("Gold2O")
+    points[Gold3]=bmap.get("Gold3O")
+    points[Gold4]=bmap.get("Gold4O")
 ss = SevenSegments(manager)
 left_eye = LEDMatrix(manager, 1)
 right_eye = LEDMatrix(manager, 2)
