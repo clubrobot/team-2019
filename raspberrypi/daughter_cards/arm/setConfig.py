2cda4bc5506f0f902c1d4f7ce2d7c50212fd96dd
Mathis Lécrivain <matlecr35@gmail.com>
2019-03-08 14:19:59 +0100

    setConfig/getConfig
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/daughter_cards/arm/setConfig.py
+++ raspberrypi/daughter_cards/arm/setConfig.py
@@ -45,27 +45,3 @@ robArm.set_origin(ORIGIN)
 robArm.set_joint(JOINTS)
 
 robArm.set_parameter_value(TIMESTEP_ID, TIMESTEP, FLOAT)
-
-print('MOTOR1 ID : '+str(robArm.get_parameter_value(MOTOR1_ID_ID, INT)))
-print('MOTOR2 ID : '+str(robArm.get_parameter_value(MOTOR2_ID_ID, INT)))
-print('MOTOR3 ID : '+str(robArm.get_parameter_value(MOTOR3_ID_ID, INT)))
-
-print('MOTOR1 OFFSET : '+str(robArm.get_parameter_value(MOTOR1_OFFSET_ID, FLOAT)))
-print('MOTOR2 OFFSET : '+str(robArm.get_parameter_value(MOTOR1_OFFSET_ID, FLOAT)))
-print('MOTOR3 OFFSET : '+str(robArm.get_parameter_value(MOTOR1_OFFSET_ID, FLOAT)))
-
-print('LINK1 LEN : '+str(robArm.get_parameter_value(LINK_1_ID, FLOAT)))
-print('LINK2 LEN : '+str(robArm.get_parameter_value(LINK_2_ID, FLOAT)))
-print('LINK3 LEN : '+str(robArm.get_parameter_value(LINK_3_ID, FLOAT)))
-print('ELBOW OR  : '+str(robArm.get_parameter_value(ELBOW_ID,  FLOAT)))
-
-print('WORKSPACE FRONT : '+str(robArm.get_workspace(WORKSPACE_FRONT_ID)))
-print('WORKSPACE BACK  : '+str(robArm.get_workspace(WORKSPACE_BACK_ID)))
-
-print('ORIGIN : '+str(robArm.get_origin()))
-print('JOINTS : '+str(robArm.get_joint()))
-
-print('TIMESTEP : '+str(robArm.get_parameter_value(TIMESTEP_ID, FLOAT)))
-
-
-
