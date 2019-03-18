9787f81c22bea1db7bd33fafbccb0d995d589057
Mathis Lécrivain <matlecr35@gmail.com>
2019-03-18 11:09:50 +0100

    update tackPuck script
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/robots/TakePuck.py
+++ raspberrypi/robots/TakePuck.py
@@ -2,7 +2,6 @@ from math import pi
 import time
 import json
 
-from common.geogebra import Geogebra
 from setup_128 import *
 
 
@@ -14,81 +13,49 @@ puck1   = test_map.get("Puck1")
 puck2   = test_map.get("Puck2")
 puck3   = test_map.get("Puck3")
 balance = test_map.get("Balance")
+path    = test_map.getall("Path_*")
+print(path)
 
-print(puck1)
-print(puck2)
-print(puck3)
-print(balance)
 
 wheeledbase.set_position(755, 2678, -pi)
 print("robot placé : ", wheeledbase.get_position())
 
-#input()
+# #input()
 
-wheeledbase.goto(*puck1, theta=pi/2)      #68.2
-while not wheeledbase.isarrived():
-    print(wheeledbase.get_position())
-    time.sleep(0.1)
-
-print("robot arrivé sur puck1")
+wheeledbase.goto(1290, 2678, theta=pi/2)
 print("robot placé : ", wheeledbase.get_position())
+time.sleep(1)
 
+wheeledbase.goto(1290, 2460, theta=pi/2)
 time.sleep(1)
 
 armController.take_puck_in_distributor()
 armController.put_in_tank()
 armController.go_home()
-##input()
 
-wheeledbase.goto(*puck2, theta=pi/2)
-while not wheeledbase.isarrived():
-    print(wheeledbase.get_position())
-    time.sleep(0.1)
-
-print("robot arrivé sur puck2")
-print("robot placé : ", wheeledbase.get_position())
-
-##input()
-
+wheeledbase.goto(1300, 2260,theta=pi/2)
+time.sleep(1)
 armController.take_puck_in_distributor()
 armController.put_in_tank()
 armController.go_home()
-#input()
 
-wheeledbase.goto(*puck3, theta=pi/2)
-while not wheeledbase.isarrived():
-    print(wheeledbase.get_position())
-    time.sleep(0.1)
-
-print("robot arrivé sur puck3")
-print("robot placé : ", wheeledbase.get_position())
-
-#input()
-
+wheeledbase.goto(1300, 2060,theta=pi/2)
+time.sleep(1)
 armController.take_puck_in_distributor()
 armController.put_in_tank()
 armController.go_home()
-#input()
 
-wheeledbase.goto(*balance, theta=pi/2)
-while not wheeledbase.isarrived():
-    print(wheeledbase.get_position())
-    time.sleep(0.1)
-
-print("robot arrivé sur Balance")
-print("robot placé : ", wheeledbase.get_position())
-
-#input()
-
+wheeledbase.goto(*balance,theta=pi/2)
+time.sleep(1)
+armController.take_in_tank()
+armController.put_in_balance()
+armController.take_in_tank()
+armController.put_in_balance()
 armController.take_in_tank()
 armController.put_in_balance()
 
-
-armController.take_in_tank()
-armController.put_in_balance()
-
-armController.take_in_tank()
-armController.put_in_balance()
 armController.go_home()
 
-print("end")
+wheeledbase.goto(755, 2678, theta=-pi)
+print("robot placé : ", wheeledbase.get_position())
+input()
