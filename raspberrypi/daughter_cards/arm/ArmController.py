7725d1da086ba10f73e3f40679b6b00f61271eb5
Mathis Lécrivain <matlecr35@gmail.com>
2019-03-18 10:59:01 +0100

    add logs into armController
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/daughter_cards/arm/ArmController.py
+++ raspberrypi/daughter_cards/arm/ArmController.py
@@ -8,86 +8,113 @@ from daughter_cards.arm.ArmPosition import *
 import math
 
 class ArmController():
-    def __init__(self, arm):
+    def __init__(self, arm, name):
         self.TankPosList 		= [TANK_POS_1, TANK_POS_2, TANK_POS_3]
         self.TankPosListTake 	= [TANK_POS_INTER_TAKE_PUCK1, TANK_POS_INTER_TAKE_PUCK2, TANK_POS_INTER_TAKE_PUCK3]
         self.TankPosListTakeBis = [TANK_POS_INTER_TAKE_PUCK1_BIS, TANK_POS_INTER_TAKE_PUCK2_BIS, TANK_POS_INTER_TAKE_PUCK3_BIS]
+        
+        self.name               = name
+        self.logger             = None
+        self.arm                = arm
+        self.CurrentTankPos     = 0
+        self.tankSize		    = 2	# size 3 by default [0, 1, 2]
+        self.Tankfull		    = False
+        self.Tankempty		    = True
 
-        self.arm = arm
-        self.CurrentTankPos = 0
-        self.tankSize		= 2	# size 3 by default [0, 1, 2]
-        self.Tankfull		= False
-        self.Tankempty		= True
+    def set_logger(self, loggger):
+        self.logger = loggger
 
+    def wait(self, timeout = 0.1):
+        while not self.arrived():
+            time.sleep(timeout)
     # private func
     def move(self, PosID):
+        self.logger(self.name, "Sending Move command: {}".format(PosID))
         self.arm.move(PosID.x, PosID.y, PosID.phi)
 
     def arrived(self):
         return self.arm.is_arrived()
 
     def start_pump(self):
+        self.logger(self.name, "Start Pump")
         self.arm.start_pump()
 
     def stop_pump(self):
+        self.logger(self.name, "Stop Pump")
         self.arm.stop_pump()
 
     def start_sluice(self):
+        self.logger(self.name, "Start Sluice")
         self.arm.start_sluice()
 
     def stop_sluice(self):
+        self.logger(self.name, "Stop Sluice")
         self.arm.stop_sluice()
 
     def setup_tank_size(self, size):
         self.tankSize = size - 1
 
     def go_home(self):
+
         self.move(GLOBAL_POS_INTER)
-        while not self.arrived():
-            time.sleep(0.1)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(GLOBAL_POS_INTER))
+
         self.move(HOME)
-        while not self.arrived():
-            time.sleep(0.1)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(HOME))
 
-    def take_puck_in_distributor(self):
-        self.start_pump()
-        self.move(TAKE_PUCK_INTER_BEFORE)
-        while not self.arrived():
-            time.sleep(0.1)
-
-        self.move(TAKE_PUCK)
-        while not self.arrived():
-            time.sleep(0.1)
         time.sleep(1)
 
+    def take_puck_in_distributor(self):
+
+        self.start_pump()
+
+        self.move(TAKE_PUCK_INTER_BEFORE)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(TAKE_PUCK_INTER_BEFORE))
+
+        self.move(TAKE_PUCK)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(TAKE_PUCK))
+
+        time.sleep(1)
+        
         self.move(TAKE_PUCK_INTER_AFTER_2)
-        while not self.arrived():
-            time.sleep(0.1)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(TAKE_PUCK_INTER_AFTER_2))
 
     def put_in_balance(self):
         self.move(BALANCE)
-        while not self.arrived():
-            time.sleep(0.1)
+        self.wait()
+        self.logger(self.name, "Arrived to : {}".format(BALANCE))
+
         time.sleep(0.5)
+
         self.stop_pump()
         self.start_sluice()
+
+        time.sleep(0.5)
 
     def put_in_tank(self):
         if not self.Tankfull:
             self.move(TANK_POS_INTER_PUT)
-            while not self.arrived():
-                time.sleep(0.1)
-			
+            self.wait()
+            self.logger(self.name, "Arrived to : {}".format(TANK_POS_INTER_PUT))
+
             self.move(self.TankPosList[self.CurrentTankPos])
+            self.wait()
+            self.logger(self.name, "Arrived to : {}".format(self.TankPosList[self.CurrentTankPos]))
 
-            while not self.arrived():
-                time.sleep(0.1)
-
+            time.sleep(1)
             self.stop_pump()
             self.start_sluice()
             time.sleep(1)
+            
             self.move(TANK_POS_INTER)
-
+            self.wait()
+            self.logger(self.name, "Arrived to : {}".format(TANK_POS_INTER))
+            
             self.CurrentTankPos += 1
             if(self.CurrentTankPos > self.tankSize):
                 self.CurrentTankPos = self.tankSize
@@ -103,22 +130,17 @@ class ArmController():
             self.start_pump()
 
             self.move(self.TankPosListTake[self.CurrentTankPos])
-
-            while not self.arrived():
-                time.sleep(0.1)
+            self.wait()
 
             self.move(self.TankPosList[self.CurrentTankPos])
-
-            while not self.arrived():
-                time.sleep(0.1)
+            self.wait()
 
             self.move(self.TankPosListTakeBis[self.CurrentTankPos])
-
-            while not self.arrived():
-                time.sleep(0.1)
+            self.wait()
 
             time.sleep(0.5)
-            self.move(TANK_POS_INTER)
+            self.move(TANK_POS_INTER_TAKE)
+            self.wait()
 
             self.CurrentTankPos -= 1
             if(self.CurrentTankPos < 0):
