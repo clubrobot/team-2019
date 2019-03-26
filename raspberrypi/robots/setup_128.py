1de2a8390d634ea177b8298ac574a6dfa7ff6cac
mathieu.percelay <mathieup@mailoo.org>
2019-03-26 14:23:06 +0100

    Cleaning robot script

--- raspberrypi/robots/setup_128.py
+++ raspberrypi/robots/setup_128.py
@@ -1,4 +1,14 @@
 from robots.setup_wheeledbase import *
+from robots.setup_serialtalks import *
 from daughter_cards.RobotArm import *
 
 arm = RobotArm(manager)
+wheeledbase = WheeledBase(manager)
+
+
+def init_robot():
+    pass
+
+
+if __name__ == "__main__":
+    init_robot()
--- raspberrypi/robots/setup_bornibus.py
+++ raspberrypi/robots/setup_bornibus.py
@@ -12,32 +12,18 @@ from robots.display_manager import *
 from math import pi
 from common.geogebra import Geogebra
 
-#bmap = Geogebra("roadmap.ggb")
-points = dict()
-
 gripper = Gripper(manager)
-gripper.open()
 pushers = Pushers(manager)
-pushers.up()
 arm = Arm(manager)
-arm.up()
-"""
-button_manager = BornibusButtonsManager(ButtonCard(manager))
-while button_manager.team is none:
-    if button_manager.team==1:
-        points[Ini]=bmap.get("IniM")
-        points[Gold1]=bmap.get("Gold1M")
-        points[Gold2]=bmap.get("Gold2M")
-        points[Gold3]=bmap.get("Gold3M")
-        points[Gold4]=bmap.get("Gold4M")
-    elif button_manager.team==2:
-        points[Ini]=bmap.get("IniO")
-        points[Gold1]=bmap.get("Gold1O")
-        points[Gold2]=bmap.get("Gold2O")
-        points[Gold3]=bmap.get("Gold3O")
-        points[Gold4]=bmap.get("Gold4O")
 ss = SevenSegments(manager)
 left_eye = LEDMatrix(manager, 1)
 right_eye = LEDMatrix(manager, 2)
 disp_man = DisplayPoints(ss, right_eye, left_eye)
-"""
+
+def init_robot():
+    gripper.open()
+    pushers.up()
+    arm.up()
+
+if __name__ == "__main__":
+    init_robot()
