66266165a6b7666b8904442501123b033c289654
Mathis Lecrivain <mlecrivain@esoftthings.com>
2019-04-03 11:51:38 +0200

    update setup128
    Signed-off-by: Mathis Lecrivain <mlecrivain@esoftthings.com>

--- raspberrypi/robots/R128/setup_128.py
+++ raspberrypi/robots/R128/setup_128.py
@@ -2,6 +2,8 @@ from robots.setup_wheeledbase import *
 from robots.setup_serialtalks import *
 from daughter_cards.arm.RobotArm import *
 from daughter_cards.arm.ArmController import *
+from daughter_cards.display import *
+from robots.display_manager import
 from common.funcutils import *
 from common.logger import *
 import time
@@ -13,6 +15,12 @@ armB = RobotArm(manager, uuid='arm_back')
 
 armFront = ArmController(armF, 'ARM FRONT', log)
 armBack  = ArmController(armB, 'ARM BACK' , log)
+
+led1 = LEDMatrix(manager, 1)
+led2 = LEDMatrix(manager, 2)
+ssd = SevenSegments(manager)
+
+disp = DisplayPoints(ssd, led1, led2)
 
 def init_robot():
         wheeledbase.set_velocities(0,0)
