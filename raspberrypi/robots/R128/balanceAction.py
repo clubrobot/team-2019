71b66bcdad75ce804caf8ed10a49aa1dd842f571
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-06 16:40:10 +0200

    add First version of balance action
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

---
+++ raspberrypi/robots/R128/balanceAction.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import time
+from math import pi
+from common.actions.action import *
+from common.funcutils      import *
+from daughter_cards.wheeledbase import *
+from daughter_cards.arm.ArmPosition import *
+
+class Balance(Actionnable):
+    YELLOW  = 0
+    PURPLE  = 1
+
+    def __init__(self, geogebra, daughter_cards, side, log):
+        self.geogebra       = geogebra
+        self.log            = log
+        self.side           = side
+
+        if self.side == Balance.YELLOW:
+            self.arm        = daughter_cards['armFront']
+        else:
+            self.arm        = daughter_cards['armBack']
+        
+        self.wheeledbase    = daughter_cards['wheeledbase']
+        # action Points
+        self.point          = self.geogebra.get('Balance{}'.format(self.side))
+        self.actionPoint    = ActPoint(self.point, pi/2)
+
+        #armPos
+        self.beforeTankPos  = [BEFORE_TAKE_TANK_PUCK1, BEFORE_TAKE_TANK_PUCK2, BEFORE_TAKE_TANK_PUCK3]
+        self.TankPos        = [TAKE_TANK_PUCK1, TAKE_TANK_PUCK2, TAKE_TANK_PUCK3]
+        self.afterTankPos   = [AFTER_TAKE_TANK_PUCK1, AFTER_TAKE_TANK_PUCK2, AFTER_TAKE_TANK_PUCK3]
+
+        self.handeledPuck   = None
+
+    def realize(self):
+        self.arm.move(BALANCE)
+        
+        while not self.arm.is_arrived():
+            time.sleep(0.1)
+
+        self.arm.stop_pump()
+    
+    def before(self):
+        self.arm.start_pump()
+        self.arm.move(self.beforeTankPos[self.arm.tank.index()-1])
+
+        while not self.arm.is_arrived():
+            time.sleep(0.1)
+
+        self.arm.move(self.TankPos[self.arm.tank.index()-1])
+
+        while not self.arm.is_arrived():
+            time.sleep(0.1)
+        
+        try:   
+            self.handeledPuck = self.arm.tank.get_puck()
+
+            self.arm.move(self.afterTankPos[self.arm.tank.index()-1])
+
+            while not self.arm.is_arrived():
+                time.sleep(0.1)
+
+            self.arm.move(TANK_POS_INTER)
+
+            while not self.arm.is_arrived():
+                time.sleep(0.1)
+        except :
+            pass
+
+    def after(self):
+        self.arm.move(TANK_POS_INTER)
+
+        while not self.arm.is_arrived():
+            time.sleep(0.1)
+
+    #override
+    def getAction(self):
+        return Action(self.actionPoint, lambda : self.realize(), self.before, self.after, 'PutBalance')
