101086b00829874fdfa9716116da5d83ed7c361c
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-07 21:22:26 +0200

    stop pump on setup
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/robots/R128/setup_128.py
+++ raspberrypi/robots/R128/setup_128.py
@@ -3,7 +3,7 @@ from robots.setup_serialtalks import *
 from daughter_cards.arm.RobotArm import *
 from daughter_cards.arm.ArmController import *
 from daughter_cards.display import *
-from robots.display_manager import
+from robots.display_manager import *
 from common.funcutils import *
 from common.logger import *
 import time
@@ -16,15 +16,18 @@ armB = RobotArm(manager, uuid='arm_back')
 armFront = ArmController(armF, 'ARM FRONT', log)
 armBack  = ArmController(armB, 'ARM BACK' , log)
 
-led1 = LEDMatrix(manager, 1)
-led2 = LEDMatrix(manager, 2)
-ssd = SevenSegments(manager)
+# led1 = LEDMatrix(manager, 1)
+# led2 = LEDMatrix(manager, 2)
+# ssd = SevenSegments(manager)
 
-disp = DisplayPoints(ssd, led1, led2)
+# disp = DisplayPoints(ssd, led1, led2)
 
-buttons = ButtonsManager()
+# buttons = ButtonsManager()
 
 def init_robot():
+        armFront.stop_pump()
+        armBack.stop_pump()
+
         armFront.go_home()
         armBack.go_home()
         while not (armFront.is_arrived() and armBack.is_arrived):
