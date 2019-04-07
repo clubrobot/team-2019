0a66f61eb8d1d1bfecc38cf50269f8603431604f
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-07 21:22:41 +0200

    add take puck action
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/robots/R128/takePuckActions.py
+++ raspberrypi/robots/R128/takePuckActions.py
@@ -7,15 +7,16 @@ from common.actions.action import *
 from common.funcutils      import *
 from daughter_cards.wheeledbase import *
 from daughter_cards.arm.ArmPosition import *
-
+from daughter_cards.arm.puckUtils import *
 class TakePuckSync(Actionnable):
     YELLOW  = 0
     PURPLE  = 1
 
-    def __init__(self, geogebra, daughter_cards, side, log):
+    def __init__(self, geogebra, daughter_cards, side, distrib_pos, log):
         self.geogebra       = geogebra
         self.log            = log
         self.side           = side
+        self.distrib_pos    = distrib_pos
 
         if self.side == self.YELLOW:
             self.arm1       = daughter_cards['armFront']
@@ -26,19 +27,62 @@ class TakePuckSync(Actionnable):
             
         self.wheeledbase    = daughter_cards['wheeledbase']
         # action Points
-        self.point          = self.geogebra.get('Balance{}'.format(self.side))
+        self.point          = self.geogebra.get('Distrib{}_{}'.format(self.side,self.distrib_pos))
         self.actionPoint    = ActPoint(self.point, pi/2)
+
+        self.TankPos        = [TAKE_TANK_PUCK1, TAKE_TANK_PUCK2, TAKE_TANK_PUCK3]
 
         self.handeledPuck   = None
 
     def realize(self):
-        pass
+        self.arm1.move(PREPARE_TAKING_POS_STATIC)
+        self.arm2.move(PREPARE_TAKING_POS_STATIC)
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
+
+        self.arm1.move(TAKE_PUCK_STATIC)
+        self.arm2.move(TAKE_PUCK_STATIC)
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
     
+        time.sleep(1)
+
+        self.arm1.move(TAKE_PUCK_INTER_AFTER_STATIC)
+        self.arm2.move(TAKE_PUCK_INTER_AFTER_STATIC)
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
+
     def before(self):
-        pass
+        self.arm1.start_pump()
+        self.arm2.start_pump()
+
+        self.arm1.move(PREPARE_TAKING_POS_ROAD)
+        self.arm2.move(PREPARE_TAKING_POS_ROAD)
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
 
     def after(self):
-        pass
+        self.arm1.move(TANK_POS_INTER)
+        self.arm2.move(TANK_POS_INTER)
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
+
+        self.arm1.move(self.TankPos[self.arm1.tank.index()])
+        self.arm2.move(self.TankPos[self.arm2.tank.index()])
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
+
+        self.arm1.tank.put_puck(GreenPuck)
+        self.arm2.tank.put_puck(GreenPuck)
+
+        self.arm1.stop_pump()
+        self.arm2.stop_pump()
+
+        time.sleep(0.5)
+        self.arm1.go_home()
+        self.arm2.go_home()
+        while not (self.arm1.is_arrived() and self.arm2.is_arrived):
+            time.sleep(0.1)
 
     #override
     def getAction(self):
