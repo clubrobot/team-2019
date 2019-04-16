488792cb43f7e988c525f013163995e740182e97
mathieu.percelay <mathieup@mailoo.org>
2019-04-16 19:50:31 +0200

    CHange in start command

--- raspberrypi/robots/Bornibus.py
+++ raspberrypi/robots/Bornibus.py
@@ -1,37 +1,29 @@
 from robots.goldenium_bornibus import *
 from robots.buttons_manager import ButtonsManager
 
-def start_match():
+points = dict()
+
+def start_preparation():
     init_robot()
     buttons = ButtonsManager()
     # couleur M ou O
-    print("wait for team")
-    ssd.set_message("set team")
-    while(buttons.state!="team selected"):
-        time.sleep(0.1)
-    print(buttons.team)
-    wheeledbase.set_velocities(0, 0)
-    couleur = buttons.team
-
-    points = map_loading(couleur)
-    ssd.clear_messages()
-    ssd.set_message("set pos")
-
-    while(buttons.state!="position selected"):
+    while buttons.state is not "OK":
         time.sleep(0.1)
 
-    if couleur == "O" :
+
+def start_robot(points):
+    start(points)
+
+
+def def_color(color):
+    if couleur == color:
         wheeledbase.set_position(*points["IniO"], pi/2)
         print("robot placé : ", wheeledbase.get_position())
 
-    if couleur == "M" :
-        wheeledbase.set_position(*points["IniO"], (3*pi)/2)
+    if couleur == color :
+        wheeledbase.set_position(*points["IniM"], (3*pi)/2)
         print("robot placé : ", wheeledbase.get_position())
 
     ssd.clear_messages()
     ssd.set_message("Ready")
-    while (buttons.state != "running"):
-        time.sleep(0.1)
-
-    start(points)
 
--- raspberrypi/robots/buttons_manager.py
+++ raspberrypi/robots/buttons_manager.py
@@ -1,37 +1,53 @@
 from robots.setup_bornibus import *
 from gpiozero import Button
 
-#GPIO: BTN_1=13 marche pas, BTN_2=12, BTN_3=6, BTN_4=5, TIR=26
+# GPIO: BTN_1=13 marche pas, BTN_2=12, BTN_3=6, BTN_4=5, TIR=26
+
 
 class ButtonsManager:
+    def set_team_orange(self):
+        self.team = "O"
+        self.color_return = self.color_funct(self.team)
+        ssd.clear_messages()
+        ssd.set_message("team : "+self.team)
+        self.green_switch.when_released = self.odometry
+
+    def set_team_purple(self):
+        self.team = "M"
+        self.color_return = self.color_funct(self.team)
+        ssd.clear_messages()
+        ssd.set_message("team : "+self.team)
+        self.green_switch.when_released = self.odometry
+
     def odometry(self):
         self.orange_switch.close()
         self.blue_switch.close()
-        self.state = "position selected"
-        self.ready()
-    def set_team_orange(self):
-        self.team = "O"
-        self.green_switch.when_pressed = self.odometry
-        self.state = "team selected"
-    def set_team_purple(self):
-        self.team = "M"
-        self.green_switch.when_pressed = self.odometry
-        self.state = "team selected"
+        self.green_switch.when_realesed = self.tirret
+
     def run_match(self):
         self.red_switch.close()
         self.green_switch.close()
-        self.tirette_switch.when_released = None
-        self.state = "running"
-    def ready(self):
-        if(self.tirette_switch.is_pressed):
-            self.tirette_switch.when_released = self.run_match
-    def __init__(self):
+        ssd.clear_messages()
+        ssd.set_message("ready")
+        self.tirette_switch.when_released = lambda: self.start_funct(self.color_return)
+
+    def tirret(self):
+        ssd.clear_messages()
+        ssd.set_message("tirret")
+        self.tirette_switch.when_pressed = self.run_match
+
+    def __init__(self, color_funct, start_funct):
         self.state = None
         self.red = 13
         self.green = 6
         self.blue = 12
         self.orange = 5
         self.tirette = 26
+
+        self.color_funct = color_funct
+        self.start_funct = start_funct
+        self.color_return = None
+        self.team = None
 
         self.red_switch = Button(self.red, pull_up=True)
         self.green_switch = Button(self.green, pull_up=True)
@@ -41,3 +57,6 @@ class ButtonsManager:
 
         self.blue_switch.when_pressed = self.set_team_purple
         self.orange_switch.when_pressed = self.set_team_orange
+
+        ssd.clear_messages()
+        ssd.set_message("set pos")
