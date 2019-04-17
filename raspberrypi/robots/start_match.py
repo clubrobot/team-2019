6ba86c6a36195c818430b68a15e29d2f9b670f92
mathieu.percelay <mathieup@mailoo.org>
2019-04-17 10:14:07 +0200

    Correction in robot start

--- raspberrypi/robots/start_match.py
+++ raspberrypi/robots/start_match.py
@@ -1,2 +1,19 @@
-from robots.setup_serialtalks import *
-manager.send(START_MATCH_OPCODE)
+from robots.buttons_manager import ButtonsManager
+
+
+def start_preparation():
+    import robots.goldenium_bornibus as Bornibus
+    manager = Bornibus.Manager()
+    manager.connect()
+    Bornibus.init_robot()
+    ButtonsManager(Bornibus.map_loading, Bornibus.def_pos, start_robot).begin()
+
+
+def start_robot(points, couleur):
+    import robots.goldenium_bornibus as Bornibus
+    try:
+        Bornibus.start(points, couleur)
+    except RuntimeError as e:
+        print("error : ", e)
+    finally:
+        Bornibus.disp.stop()
