0f41004bcb4bd52f6d1f300e6346ca5484b557db
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-18 13:25:20 +0200

    hello
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/robots/R128/movingAction.py
+++ raspberrypi/robots/R128/movingAction.py
@@ -81,8 +81,10 @@ class MovingAfterStart(Actionnable):
             self.sensor_manager.disable_back()
 
     def realize(self):
-        self.wheeledbase.goto(*self.point, pi/2, direction = 'backward')
-    
+        if self.side == self.YELLOW:
+            self.wheeledbase.goto(*self.point, pi/2, direction = 'backward')
+        else:
+            self.wheeledbase.goto(*self.point, pi/2, direction = 'forward')
     def before(self):
         pass
 
