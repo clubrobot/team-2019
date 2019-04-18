25a1ebae6087b4b9925af9a5cff5128955979cfa
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-18 13:36:19 +0200

    m
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- raspberrypi/robots/R128/movingAction.py
+++ raspberrypi/robots/R128/movingAction.py
@@ -82,9 +82,9 @@ class MovingAfterStart(Actionnable):
 
     def realize(self):
         if self.side == self.YELLOW:
-            self.wheeledbase.goto(*self.point, pi/2, direction = 'backward')
+            self.wheeledbase.goto(*self.point, pi/2, direction = 'forward')
         else:
-            self.wheeledbase.goto(*self.point, pi/2)
+            self.wheeledbase.goto(*self.point, pi/2, direction = 'backward')
     def before(self):
         pass
 
