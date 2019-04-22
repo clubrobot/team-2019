cb11459e26cc11439dab0b017a913227c8f07182
mathieu.percelay <mathieup@mailoo.org>
2019-04-22 20:49:42 +0200

    Change structure of match manager by automaton

--- raspberrypi/robots/automaton.py
+++ raspberrypi/robots/automaton.py
@@ -1,3 +1,5 @@
+from robots.get_robot_name import *
+
 class Automaton:
     PURPLE = 0
     YELLOW = 1
@@ -14,3 +16,26 @@ class Automaton:
 
     def run(self):
         pass
+
+    @staticmethod
+    def start_preparation():
+        from common.components import Manager
+        manager = Manager()
+        manager.connect()
+        import os
+        os.chdir("/home/pi/git/clubrobot/team-2019")
+        from robots.buttons_manager import ButtonsManager
+
+        if ROBOT_ID == BORNIBUS_ID:
+            print("Launch match Bornibus")
+            from robots.Bornibus.Bornibus import Bornibus
+            auto = Bornibus()
+            print("Button manager Bornibus")
+        else:
+            print("Launch match 128")
+            from robots.R128.R128 import R128
+            print("Init 128")
+            auto = R128()
+        print("Button manager")
+        ButtonsManager(auto).begin()
+
