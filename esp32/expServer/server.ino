0751f39bce1ef7c8d4d80bfe525fd061e25f2905
Mathis Lécrivain <matlecr35@gmail.com>
2019-04-26 19:55:41 +0200

    server AP
    Signed-off-by: Mathis Lécrivain <matlecr35@gmail.com>

--- esp32/expServer/server.ino
+++ esp32/expServer/server.ino
@@ -1,86 +1,57 @@
-#include <Arduino.h>
-#include "../common/SerialTalks.h"
-#include "instructions.h"
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
 
-#define SERVICE_UUID      "6a54cace-89bf-4e0b-a9f9-7f78a7aab1ef"
-#define STARTCHAR_UUID    "bc4878c0-426a-45ed-b5db-2a9e1a1e43d7"
-#define ISONTOP_UUID      "561a3414-8b23-462e-ab59-1cdb47905789"
+// Load Wi-Fi library
+#include <WiFi.h>
 
-BLECharacteristic *pStartCharacteristic;
-BLECharacteristic *pIsOnTopCharacteristic;
-boolean deviceConnected = false;
+// Replace with your network credentials
+const char* ssid     = "EXP AP";
+const char* password = "zigouigoui";
 
-class MyServerCallbacks : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-    BLEDevice::startAdvertising();
+// Set web server port number to 80
+WiFiServer server(1883);
 
-  };
+// Set your Static IP address
+IPAddress local_IP(192, 168, 1, 13);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
 
-  void onDisconnect(BLEServer *pServer)
-  {
-    deviceConnected = false;
-  }
-};
+IPAddress subnet(255, 255, 0, 0);
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
+IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
-class MyCallbacks : public BLECharacteristicCallbacks{
-  void onWrite(BLECharacteristic *mCharacteristic){
-    std::string rxValue = mCharacteristic->getValue();
-    int dataLength = rxValue.length();
-    Serial.print("Received value (" );
-    Serial.print( dataLength );
-    Serial.println( " bytes)");
-    Serial.print("Received : ");
-    char rxData[dataLength];
-    for(int i = 0; i < dataLength; ++i){
-      rxData[i] = rxValue[i];
-      Serial.print(rxData[i]);
-    }
-    Serial.println("");
-    //Serial.println(rxData);
-  }
-};
+void setup() {
+  Serial.begin(115200);
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
 
-void setup()
-{
-
-
-  BLEDevice::init("Server");
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pStartCharacteristic = pService->createCharacteristic(STARTCHAR_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-  pIsOnTopCharacteristic = pService->createCharacteristic(ISONTOP_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-
-  pStartCharacteristic->setCallbacks(new MyCallbacks());
-  pIsOnTopCharacteristic->setCallbacks(new MyCallbacks());
-  pStartCharacteristic->setValue("0");
-  pIsOnTopCharacteristic->setValue("0");
-  pService->start();
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
-  pAdvertising->setMinPreferred(0x12);
-  BLEDevice::startAdvertising();
-
-  Serial.begin(SERIALTALKS_BAUDRATE);
-  talks.begin(Serial);
-
-  talks.bind(START_OPCODE, START);
-  talks.bind(ISONTOP_OPCODE, ISONTOP);
-
-
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  
+  server.begin();
 }
 
-void loop()
-{
-   talks.execute();
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
+  if (client) 
+  {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+
+    while (client.connected()) 
+    {
+      if (client.available()) 
+      {
+      
+      }
+    }
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
