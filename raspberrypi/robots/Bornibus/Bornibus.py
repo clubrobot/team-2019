fa211a1923947c08ef3a6f70d8fce386261466ef
GuillaumeDLT <guillaumedeltombe@orange.fr>
2019-05-02 22:43:59 +0200

    First action of Bornibus rencoded for intelligence

--- raspberrypi/robots/Bornibus/Bornibus.py
+++ raspberrypi/robots/Bornibus/Bornibus.py
@@ -30,7 +30,7 @@ class Bornibus(Automaton):
                                     arm             = arm,
                                     endstops        = endstops,
                                     gripper         = gripper,
-                                    display         = disp)
+                                    display         = disp )
 
         # Save annexes inf
         self.side           = Automaton.UNDEFINED
@@ -51,23 +51,12 @@ class Bornibus(Automaton):
 
         self.points = dict()
 
-    def set_position(self):
-        if self.side == Bornibus.PURPLE:
-            wheeledbase.set_position(*self.points["Ini"], -pi/2)
-            print(*self.points["Ini"])
-        if self.side == Bornibus.YELLOW:
-            wheeledbase.set_position(*self.points["Ini"], pi/2)
-            print(*self.points["Ini"])
-
     def set_side(self, side):
         self.side = side
-        color = "M" if side == self.PURPLE else "O"
+        self.log("SIDE CONFIG : ", "Set Side : {}".format(self.side))
+        color = "P" if side == self.PURPLE else "Y"
 
         self.points["Ini"] = geo.get("Ini"+color)
-
-        self.points["Dep1"] = geo.get("Dep1"+color)
-        self.points["Dep2"] = geo.get("Dep2"+color)
-        self.points["Dep3"] = geo.get("Dep3"+color)
 
         self.points["Gold2"] = geo.get("Gold2"+color)
         self.points["Gold3"] = geo.get("Gold3"+color)
@@ -82,18 +71,20 @@ class Bornibus(Automaton):
         self.points["Pal5"] = geo.get("Pal5"+color)
         self.points["Pal6"] = geo.get("Pal6"+color)
 
+        # Specific Actions initialisation
+        self.detectorAct    = detector(self.geogebra, self.daughter_cards, self.side, self.log).getAction()
+        
         self.action_list = [
-            self.takeSyncPos1Act,
-            self.takeSyncPos2Act,
-            self.takeSyncPos3Act,
-            self.balanceAct6,
-            self.movingTolittle,
-            self.takesingle,
-            self.takemaintain,
-            self.movingAfterlittle,
-            self.balanceAct3,
-            self.putRedZoneAct,
+            self.detectorAct
         ]
+
+    def set_position(self):
+        if self.side == Bornibus.PURPLE:
+            wheeledbase.set_position(*self.points["Ini"], -pi/2)
+            print(*self.points["Ini"])
+        if self.side == Bornibus.YELLOW:
+            wheeledbase.set_position(*self.points["Ini"], pi/2)
+            print(*self.points["Ini"])
 
     def stop_match(self):
         import time
@@ -105,90 +96,58 @@ class Bornibus(Automaton):
         manager.disconnect()
 
     def run(self):
+
+        self.log("MAIN : ", "RUN...")
+        self.log.reset_time()
         Thread(target=self.stop_match).start()
+        self.display.start()
         begin = time.time()
 
         sens_manager.start()
         wheeledbase.reset_parameters()
-        wheeledbase.max_linvel.set(700)
-        wheeledbase.max_angvel.set(10)
-        wheeledbase.lookahead.set(150.0)
-        wheeledbase.max_linacc.set(500.0)
-        wheeledbase.max_lindec.set(700.0)
-
-        print("max lin vel : ", wheeledbase.max_linvel.get())
-        print("max ang vel : ", wheeledbase.max_angvel.get())
-
-        print("disable back")
-        # sens_manager.disable_back()
-        # sens_manager.set_thresold(150)
-        print("sens_manager start")
-        # sens_manager.start()
-        # time.sleep(2)
-        print("max ang vel : ", wheeledbase.max_angvel.get())
-        print("max lin vel : ", wheeledbase.max_linvel.get())
-
         disp.points = 0
         disp.start()
-
         pushers.up()
-        gripper.open()
+        gripper.open()    
+
+        print("INTO RUN ...")
+        time.sleep(3)
+        input()
+
+        self.tam.start()
         
-        # Vers l'accélérateur
-        print("Vers l'accélérateur")
-        wheeledbase.purepursuit([wheeledbase.get_position()[:2], self.points["Dep1"], self.points["Dep2"],
-                                 self.points["Dep3"], self.points["Gold2"]], direction="forward", lookahead=150, lookaheadbis=3)
-        wheeledbase.wait()
-        print(wheeledbase.get_position())
-        wheeledbase.turnonthespot(0)
-        wheeledbase.wait()
-        wheeledbase.set_velocities(-200, 0)
-        time.sleep(2)
-        wheeledbase.set_velocities(0, 0)
-
-        if self.side == Bornibus.YELLOW :
-            wheeledbase.goto_delta(100, 30)
+        for act in self.action_list:
             
-        if self.side == Bornibus.PURPLE :
-            wheeledbase.goto_delta(100, -30)
+            # add before action to the parralel action queue
+            self.log("MAIN : ", "Launch Before Action")
+            self.tam.putAction(act.getBefore())
+
+            # moving to action point
+            self.log("MAIN : ", "Moving ! ...")
+            act.moving()
+
+            # wait for parralels action end
+            while not self.tam.end():
+                time.sleep(0.1)
             
-        wheeledbase.wait()
+            # execute the current action
+            self.log("MAIN ; ", "Arrived on action point ! Go execute {} =)".format(act.name))
+            act()
+            act.done.set()
+            self.log("MAIN ; ", "Action End !")
 
-        # wheeledbase.turnonthespot(-2*pi/3)
-        # wheeledbase.wait()
-        # arm.up()
-        wheeledbase.lookaheadbis.set(150)
+            # add after action to the parrel action queue
+            self.log("MAIN : ", "Launch After Action")
+            self.tam.putAction(act.getAfter())
+            
+            self.log("MAIN : ", "Let's go to the next action !")
+        
+        #stop thread action manager
+        self.tam.stop()
+        self.display.stop()
+        self.wheeledbase.stop()
 
-        # Prépare le bras
-        print("Prépare le bras")
-        # sens_manager.disable_front()
-        if self.side == Bornibus.YELLOW:
-            arm.deploy()
-            wheeledbase.turnonthespot(-2*pi/3)
-            while not wheeledbase.isarrived():
-                time.sleep(0.1)
-        if self.side == Bornibus.PURPLE:
-            wheeledbase.turnonthespot(-3*pi/4)
-            wheeledbase.wait()
-            arm.deploy()
-            wheeledbase.turnonthespot(-pi/3)
-            while not wheeledbase.isarrived():
-                time.sleep(0.1)
-
-        time.sleep(0.5)
-        arm.deploy()
-
-        # Pousse
-        print("Pousse")
-        wheeledbase.turnonthespot(-pi/2)
-        while not wheeledbase.isarrived():
-            print(wheeledbase.get_position())
-            time.sleep(0.1)
-
-        arm.up()
-        disp.addPoints(20)
-        gripper.open()
-
+        """
         # Vers préparation Goldenium
         print("Vers préparation Goldenium")
         # sens_manager.enable_front()
@@ -351,6 +310,7 @@ class Bornibus(Automaton):
         wheeledbase.stop()
         print("fin")
         disp.stop()
+        """
 
 if __name__ == "__main__":
     auto = Bornibus()
---
+++ raspberrypi/robots/Bornibus/actions/detectorAction.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+import time
+from math import pi
+from common.actions.action import *
+from common.funcutils      import *
+from common.geogebra import Geogebra
+from daughter_cards.wheeledbase import *
+
+class detector(Actionnable):
+    YELLOW  = 0
+    PURPLE  = 1
+    def __init__(self, geogebra, daughter_cards, side, log):
+        self.geogebra       = geogebra
+        self.log            = log
+        self.side           = side
+
+        self.wheeledbase    = daughter_cards['wheeledbase']
+        self.display        = daughter_cards['display']
+        self.arm            = daughter_cards['arm']
+
+        if self.side == self.YELLOW:
+            color = "Y"
+        else:
+            color = "P"
+
+        # action Points
+        self.points = dict()
+        self.points["Dep1"] = self.geogebra.get("Dep1"+color)
+        self.points["Dep2"] = self.geogebra.get("Dep2"+color)
+        self.points["Dep3"] = self.geogebra.get("Dep3"+color)
+        self.points["Gold2"] = self.geogebra.get("Gold2"+color)
+
+
+    def moving(self):
+        self.wheeledbase.max_linvel.set(700)
+        self.wheeledbase.max_angvel.set(10)
+        self.wheeledbase.lookahead.set(150.0)
+        self.wheeledbase.max_linacc.set(500.0)
+        self.wheeledbase.max_lindec.set(700.0)
+
+        # Vers l'accélérateur
+        print("--- Vers l'accélérateur")
+
+        self.wheeledbase.purepursuit([self.wheeledbase.get_position()[:2], self.points["Dep1"], self.points["Dep2"],
+                                 self.points["Dep3"], self.points["Gold2"]], direction="forward", lookahead=150, lookaheadbis=3)
+        self.wheeledbase.wait()
+        self.wheeledbase.turnonthespot(0)
+        self.wheeledbase.wait()
+        self.wheeledbase.right_wheel_maxPWM.set(0.2)
+        self.wheeledbase.left_wheel_maxPWM.set(0.2)
+        self.wheeledbase.set_velocities(-200, 0)
+        time.sleep(2)
+        self.wheeledbase.set_velocities(0, 0)
+        self.wheeledbase.right_wheel_maxPWM.set(1)
+        self.wheeledbase.left_wheel_maxPWM.set(1)
+
+        if self.side == self.YELLOW :
+            self.wheeledbase.goto_delta(100, 30)
+            
+        if self.side == self.PURPLE :
+            self.wheeledbase.goto_delta(100, -30)            
+        self.wheeledbase.wait()
+        self.wheeledbase.lookaheadbis.set(150)
+
+    def realize(self):
+        # Prépare le bras
+        print("Prépare le bras")
+
+        if self.side == self.YELLOW:
+            self.arm.deploy()
+            self.wheeledbase.turnonthespot(-2*pi/3)
+            while not self.wheeledbase.isarrived():
+                time.sleep(0.1)
+        if self.side == self.PURPLE:
+            self.wheeledbase.turnonthespot(-3*pi/4)
+            self.wheeledbase.wait()
+            self.arm.deploy()
+            self.wheeledbase.turnonthespot(-pi/3)
+            while not self.wheeledbase.isarrived():
+                time.sleep(0.1)
+
+        time.sleep(0.5)
+        self.arm.up()
+
+        # Pousse le Blueium
+        print("Pousse le Blueium")
+        self.wheeledbase.turnonthespot(-pi/2)
+        while not self.wheeledbase.isarrived():
+            print(self.wheeledbase.get_position())
+            time.sleep(0.1)
+        self.arm.up()
+        self.display.addPoints(20)
+    
+    def before(self):
+        pass
+
+    def after(self):
+        pass
+
+    #override
+    def getAction(self):
+        return Action(lambda : self.moving(), lambda : self.realize(), self.before, self.after, 'detectorAction')
