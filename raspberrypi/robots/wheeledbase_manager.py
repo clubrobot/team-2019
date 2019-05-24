caed7d33024fcca252fc7777db98eb3ef5d5b61a
mathieu.percelay <mathieup@mailoo.org>
2019-05-24 13:50:12 +0200

    merge

--- raspberrypi/robots/wheeledbase_manager.py
+++ raspberrypi/robots/wheeledbase_manager.py
@@ -4,15 +4,17 @@ from math import copysign, hypot, pi
 from common.sync_flag_signal import Flag
 from time import sleep
 from threading import RLock, Event
+
+
 class Mover:
-    QUICK  = 0
-    SLOW   = 1
+    QUICK = 0
+    SLOW = 1
     MEDIUM = 2
 
     FORWARD = 0
     BACKWARD = 1
 
-    LEFT  = 0
+    LEFT = 0
     RIGHT = 1
 
     LATERAL_SHIFT = 300
@@ -21,15 +23,15 @@ class Mover:
 
     def __init__(self, wheeledbase, log_meth, sensorsFront, sensorsBack):
         self.wheeledbase = wheeledbase
-        
+
         self.logger = log_meth
         self.front_center = SensorListener(sensorsFront[1])
-        self.front_left   = SensorListener(sensorsFront[2])
-        self.front_right  = SensorListener(sensorsFront[0])
+        self.front_left = SensorListener(sensorsFront[2])
+        self.front_right = SensorListener(sensorsFront[0])
 
         self.back_center = SensorListener(sensorsBack[1])
-        self.back_left   = SensorListener(sensorsBack[2])
-        self.back_right  = SensorListener(sensorsBack[0])
+        self.back_left = SensorListener(sensorsBack[2])
+        self.back_right = SensorListener(sensorsBack[0])
 
         self.front_flag = Flag(self.front_obstacle)
         self.left_flag = Flag(lambda: self.lateral_obstacle(self.LEFT))
@@ -42,12 +44,12 @@ class Mover:
         self.goto_interrupt = Event()
         self.running = Event()
         self.direction = "forward"
-        self.params ={}
+        self.params = {}
         self.goal = (0, 0, 0)
         self.timeout = 1
 
     def front_obstacle(self):
-        #interuption quand obstacle devant:
+        # interuption quand obstacle devant:
         self.logger("MOVER : ", "Object in the front detected !")
         if not self.interupted_lock.acquire(blocking=True, timeout=0.5):
             self.logger("MOVER : ", "Abort !")
@@ -57,7 +59,7 @@ class Mover:
         self.logger("MOVER : ", "Waiting for a second !")
         sleep(1)
         x, y, _ = self.wheeledbase.get_position()
-        self.wheeledbase.purepursuit(((x,y),(self.goal)),**self.params)
+        self.wheeledbase.purepursuit(((x, y), (self.goal)), **self.params)
 
         self.interupted_status.clear()
         self.interupted_lock.release()
@@ -77,9 +79,9 @@ class Mover:
         sleep(2)
         x, y, theta = self.wheeledbase.get_position()
         if side == self.LEFT:
-            lateral_point = (x + math.sin(theta)*self.LATERAL_SHIFT, y + math.cos(theta)*self.LATERAL_SHIFT)
+            lateral_point = (x + math.sin(theta) * self.LATERAL_SHIFT, y + math.cos(theta) * self.LATERAL_SHIFT)
         elif side == self.RIGHT:
-            lateral_point = (x - math.sin(theta)*self.LATERAL_SHIFT, y - math.cos(theta)*self.LATERAL_SHIFT)
+            lateral_point = (x - math.sin(theta) * self.LATERAL_SHIFT, y - math.cos(theta) * self.LATERAL_SHIFT)
         else:
             lateral_point = (x, y)
 
@@ -104,8 +106,8 @@ class Mover:
     def goto(self, x, y, **params):
         self.params = params
         self.goal = (x, y)
-        #TODO ami
-        if False :
+        # TODO ami
+        if False:
             self.in_path_flag.bind(self.friend_listener.signal)
         if self.params.get("direction") is 'forward' or self.params.get("direction") is None:
             self.logger("MOVER : ", "forward")
@@ -123,12 +125,12 @@ class Mover:
 
         self.isarrived = False
         x, y, _ = self.wheeledbase.get_position()
-        self.logger("MOVER : ", "path ", ((x,y),(self.goal)))
-        self.wheeledbase.purepursuit(((x,y),(self.goal)),**self.params)
+        self.logger("MOVER : ", "path ", ((x, y), (self.goal)))
+        self.wheeledbase.purepursuit(((x, y), (self.goal)), **self.params)
         # while hypot(x-self.goal[0], y-self.goal[1])>100:
         while not self.isarrived or self.interupted_status.is_set():
             try:
-                if(self.goto_interrupt.is_set()):
+                if (self.goto_interrupt.is_set()):
                     break
 
                 self.isarrived = self.wheeledbase.isarrived()
@@ -138,20 +140,20 @@ class Mover:
                     while self.interupted_status.is_set():
                         sleep(0.1)
                     continue
-                #self.logger("MOVER : ", "Spin! ")
+                # self.logger("MOVER : ", "Spin! ")
                 x, y, _ = self.wheeledbase.get_position()
                 vel, ang = self.wheeledbase.get_velocities_wanted(True)
                 self.wheeledbase.set_velocities(copysign(150, -vel), copysign(1, ang))
                 time.sleep(1)  # 0.5
                 self.wheeledbase.set_velocities(copysign(150, vel), 0)
                 time.sleep(1.2)
-                self.wheeledbase.purepursuit(((x,y),(self.goal)),**self.params)
+                self.wheeledbase.purepursuit(((x, y), (self.goal)), **self.params)
                 self.interupted_lock.release()
             except TimeoutError:
                 self.isarrived = False
 
         # self.on_path_flag.clear()
-        
+
         self.reset()
         if (self.goto_interrupt.is_set()):
             raise RuntimeError()
